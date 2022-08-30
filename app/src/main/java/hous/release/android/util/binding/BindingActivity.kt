ad9d235a2b5f00748bac30dc6f7bb9b583480d24
murjune <sis9221302@gmail.com>
2022-08-31 01:42:38 +0900

    [FEAT] BindingActivity, BindingFragment 추가

---
+++ app/src/main/java/hous/release/android/util/binding/BindingActivity.kt
@@ -0,0 +1,18 @@
+package hous.release.android.util.binding
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BindingActivity<T : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+    }
+}
