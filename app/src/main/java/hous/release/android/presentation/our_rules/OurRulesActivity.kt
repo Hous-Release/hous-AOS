627cfac390bb153cd803004f4785db0a19ad34c6
murjune <sis9221302@gmail.com>
2022-08-31 01:13:46 +0900

    [ADD] OurRuleActivity, xml 생성

---
+++ app/src/main/java/hous/release/android/presentation/our_rules/OurRulesActivity.kt
@@ -0,0 +1,12 @@
+package hous.release.android.presentation.our_rules
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hous.release.android.R
+
+class OurRulesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_our_rules)
+    }
+}
