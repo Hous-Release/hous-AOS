e0198b323d6081f77129f3f7fe03104ee8ad7bc4
murjune <sis9221302@gmail.com>
2022-09-02 21:25:33 +0900

    #7 [chore] @AndroidEntryPoint 추가

--- app/src/main/java/hous/release/android/presentation/our_rules/OurRulesActivity.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/OurRulesActivity.kt
@@ -2,8 +2,10 @@ package hous.release.android.presentation.our_rules
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 
+@AndroidEntryPoint
 class OurRulesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
