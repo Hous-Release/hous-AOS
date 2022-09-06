8408b1d1672419c2360ce930e647c257dc5d607b
이빵주 <yungju9982@naver.com>
2022-09-06 23:41:35 +0900

    #4 [add] 네이티브 앱 키 초기화

--- app/src/main/java/hous/release/android/HousApplication.kt
+++ app/src/main/java/hous/release/android/HousApplication.kt
@@ -1,6 +1,7 @@
 package hous.release.android
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import hous.release.android.util.binding.HousDeBugTree
 import timber.log.Timber
@@ -9,6 +10,7 @@ import timber.log.Timber
 class HousApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, resources.getString(R.string.kakao_native_app_key))
         if (BuildConfig.DEBUG) Timber.plant(HousDeBugTree())
     }
 }
