c253cf37560965be2a3cd6c58e2514f7520f8b77
이빵주 <yungju9982@naver.com>
2022-09-06 23:06:20 +0900

    #4 [add] 네이티브 앱 키 초기화

--- app/src/main/java/hous/release/android/GlobalApplication.kt
+++ app/src/main/java/hous/release/android/GlobalApplication.kt
@@ -1,4 +1,11 @@
 package hous.release.android
 
-class GlobalApplication {
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, "23a6d7ad94f44f0e474ee41b4e6d9fab")
+    }
 }
