7d56e6505a3f57237bed4d16878fd6e390b8221f
Yunjoo Son <thsduswn4141@naver.com>
2022-09-07 17:59:37 +0900

    #10 [feat] BottomNavigation에 Hous-, To-Do, Profile 화면 연결

--- app/src/main/java/hous/release/android/presentation/main/MainActivity.kt
+++ app/src/main/java/hous/release/android/presentation/main/MainActivity.kt
@@ -2,11 +2,27 @@ package hous.release.android.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import hous.release.android.R
+import hous.release.android.databinding.ActivityMainBinding
+import hous.release.android.util.binding.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        val navController = findNavController()
+        binding.botNavMain.setupWithNavController(navController)
+    }
+
+    private fun findNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
+        return navHostFragment.navController
     }
 }
