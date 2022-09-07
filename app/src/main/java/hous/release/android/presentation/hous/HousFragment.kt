942439ad960c5f5cf017a1e317bd78e79837eac8
Yunjoo Son <thsduswn4141@naver.com>
2022-09-07 17:28:01 +0900

    #10 [ui] MainActivity BottomNavigation 디자인 설정

--- app/src/main/java/hous/release/android/presentation/hous/HousFragment.kt
+++ app/src/main/java/hous/release/android/presentation/hous/HousFragment.kt
@@ -1,12 +1,12 @@
-package hous.release.android.presentation.home
+package hous.release.android.presentation.hous
 
 import android.os.Bundle
 import android.view.View
 import hous.release.android.R
-import hous.release.android.databinding.FragmentHomeBinding
+import hous.release.android.databinding.FragmentHousBinding
 import hous.release.android.util.binding.BindingFragment
 
-class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HousFragment : BindingFragment<FragmentHousBinding>(R.layout.fragment_hous) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
