d369f39ad7f47b2e7537d7ecc1892275534bc638
Yunjoo Son <thsduswn4141@naver.com>
2022-09-09 01:51:17 +0900

    #13 [feat] 방만들기, 초대코드 입력하기 화면 뒤로가기 구현

--- app/src/main/java/hous/release/android/presentation/enter_room/CreateRoomFragment.kt
+++ app/src/main/java/hous/release/android/presentation/enter_room/CreateRoomFragment.kt
@@ -1,8 +1,20 @@
 package hous.release.android.presentation.enter_room
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import hous.release.android.R
 import hous.release.android.databinding.FragmentCreateRoomBinding
 import hous.release.android.util.binding.BindingFragment
 
 class CreateRoomFragment :
-    BindingFragment<FragmentCreateRoomBinding>(R.layout.fragment_create_room)
+    BindingFragment<FragmentCreateRoomBinding>(R.layout.fragment_create_room) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBackBtnClickListener()
+    }
+
+    private fun initBackBtnClickListener() {
+        binding.btnCreateRoomBack.setOnClickListener { findNavController().popBackStack() }
+    }
+}
