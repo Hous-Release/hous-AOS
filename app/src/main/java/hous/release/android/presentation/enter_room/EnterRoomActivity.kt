750003f0a3507c68964d4023b0c0f04518a75826
Yunjoo Son <thsduswn4141@naver.com>
2022-09-09 00:37:52 +0900

    #13 [feat] 방 만들기/입장 관련 뷰 이동 구현

--- app/src/main/java/hous/release/android/presentation/enter_room/EnterRoomActivity.kt
+++ app/src/main/java/hous/release/android/presentation/enter_room/EnterRoomActivity.kt
@@ -1,12 +1,7 @@
 package hous.release.android.presentation.enter_room
 
-import android.os.Bundle
 import hous.release.android.R
 import hous.release.android.databinding.ActivityEnterRoomBinding
 import hous.release.android.util.binding.BindingActivity
 
-class EnterRoomActivity : BindingActivity<ActivityEnterRoomBinding>(R.layout.activity_enter_room) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class EnterRoomActivity : BindingActivity<ActivityEnterRoomBinding>(R.layout.activity_enter_room)
