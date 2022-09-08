a3f0b1ab45a13d8685a07bcefdfe9b403a11317b
Yunjoo Son <thsduswn4141@naver.com>
2022-09-09 06:25:16 +0900

    #17 [feat] Hilt 의존성 주입 모듈 생성

---
+++ app/src/main/java/hous/release/android/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package hous.release.android.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+}
