2c5b780f1e4a5aef64e4acb6fa69517498b0db06
이빵주 <yungju9982@naver.com>
2022-09-18 23:54:19 +0900

    #14 [add] AuthRepositoryImpl에 postLogin 함수 추가

--- data/src/main/java/hous/release/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/hous/release/data/repository/AuthRepositoryImpl.kt
@@ -1,9 +1,40 @@
 package hous.release.data.repository
 
-import hous.release.data.source.AuthDataSource
+import hous.release.data.datasource.AuthDataSource
+import hous.release.data.entity.request.LoginRequest
+import hous.release.domain.entity.request.DomainLoginRequest
+import hous.release.domain.entity.response.DomainLoginResponse
+import hous.release.domain.entity.response.Token
 import hous.release.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authDataSource: AuthDataSource
-) : AuthRepository
+) : AuthRepository {
+    override suspend fun postLogin(loginRequest: DomainLoginRequest): Result<DomainLoginResponse> {
+        kotlin.runCatching {
+            authDataSource.postLogin(
+                LoginRequest(
+                    fcmToken = loginRequest.fcmToken,
+                    socialType = loginRequest.socialType,
+                    token = loginRequest.token
+                )
+            )
+        }.onSuccess { response ->
+            return Result.success(
+                DomainLoginResponse(
+                    token = Token(
+                        response.data.token.accessToken,
+                        response.data.token.refreshToken
+                    ),
+                    userId = response.data.userId
+                )
+            )
+        }.onFailure { throw it }
+        throw IllegalStateException(UNKNOWN_ERROR)
+    }
+
+    companion object {
+        const val UNKNOWN_ERROR = "네트워크 통신 중 알 수 없는 오류"
+    }
+}
