0d691fb879eb9a83e2fb156a4d24225fa4fef698
이빵주 <yungju9982@naver.com>
2022-09-18 23:37:35 +0900

    #14 [add] AuthRepository에 postLogin 생성

--- domain/src/main/java/hous/release/domain/repository/AuthRepository.kt
+++ domain/src/main/java/hous/release/domain/repository/AuthRepository.kt
@@ -1,4 +1,8 @@
 package hous.release.domain.repository
 
+import hous.release.domain.entity.request.DomainLoginRequest
+import hous.release.domain.entity.response.DomainLoginResponse
+
 interface AuthRepository {
+    suspend fun postLogin(loginRequest: DomainLoginRequest): Result<DomainLoginResponse>
 }
