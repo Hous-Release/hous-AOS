0ffb87e19ef6bf97d1ca26e0cbea3d4304a8e525
이빵주 <yungju9982@naver.com>
2022-09-18 23:46:57 +0900

    #14 [fix] AuthService 함수명, body, Response 클래스명 변경

--- data/src/main/java/hous/release/data/service/AuthService.kt
+++ data/src/main/java/hous/release/data/service/AuthService.kt
@@ -1,13 +1,14 @@
 package hous.release.data.service
 
-import hous.release.data.model.WrapperClass
-import hous.release.data.model.request.LoginRequest
+import hous.release.data.entity.response.BaseResponse
+import hous.release.data.entity.response.LoginResponse
+import hous.release.domain.entity.request.DomainLoginRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("v1/auth/login")
-    suspend fun kakaoLogin(
-        @Body body: LoginRequest
-    ): WrapperClass<Any>
+    @POST("/v1/auth/login")
+    suspend fun postLogin(
+        @Body body: DomainLoginRequest
+    ): BaseResponse<LoginResponse>
 }
