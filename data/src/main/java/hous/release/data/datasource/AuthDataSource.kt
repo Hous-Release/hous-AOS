0ee1288ba1ea5f19c8feab4700498a1b3d30af6e
이빵주 <yungju9982@naver.com>
2022-09-18 23:40:54 +0900

    #14 [add] AuthDataSource 생성

---
+++ data/src/main/java/hous/release/data/datasource/AuthDataSource.kt
@@ -0,0 +1,13 @@
+package hous.release.data.datasource
+
+import hous.release.data.entity.request.LoginRequest
+import hous.release.data.entity.response.BaseResponse
+import hous.release.data.entity.response.LoginResponse
+import hous.release.data.service.AuthService
+import hous.release.domain.entity.request.DomainLoginRequest
+import javax.inject.Inject
+
+class AuthDataSource @Inject constructor(
+    private val authService: AuthService
+) {
+}
--- data/src/main/java/hous/release/data/source/AuthDataSource.kt
+++
@@ -1,8 +0,0 @@
-package hous.release.data.source
-
-import hous.release.data.model.WrapperClass
-import hous.release.data.model.request.LoginRequest
-
-interface AuthDataSource {
-    suspend fun login(loginRequest: LoginRequest): WrapperClass<Any>
-}
