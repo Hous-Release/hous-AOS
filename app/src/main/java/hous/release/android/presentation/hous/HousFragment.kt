d5e87d26daa0b3fdcf4392de22a98f062b63d444
murjune <sis9221302@gmail.com>
2022-09-21 06:52:48 +0900

    #26 [delete] Test하기 위한 OurRulesActivity 로 navigate로직 삭제

--- app/src/main/java/hous/release/android/presentation/hous/HousFragment.kt
+++ app/src/main/java/hous/release/android/presentation/hous/HousFragment.kt
@@ -1,19 +1,13 @@
 package hous.release.android.presentation.hous
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import hous.release.android.R
 import hous.release.android.databinding.FragmentHousBinding
-import hous.release.android.presentation.our_rules.OurRulesActivity
 import hous.release.android.util.binding.BindingFragment
 
 class HousFragment : BindingFragment<FragmentHousBinding>(R.layout.fragment_hous) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO 테스트해보고 지우기!!
-        Log.d("로그", "HousFragment - onViewCreated() called")
-        startActivity(Intent(context, OurRulesActivity::class.java))
     }
 }
