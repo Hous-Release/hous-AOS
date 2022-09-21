fd7524293dcd14e75bed90b20d5722c0fab45edb
이빵주 <yungju9982@naver.com>
2022-09-21 22:46:45 +0900

    #14 [fix] authService의 postLogin 파라미터 수정

--- data/src/main/java/hous/release/data/datasource/AuthDataSource.kt
+++ data/src/main/java/hous/release/data/datasource/AuthDataSource.kt
@@ -4,18 +4,11 @@ import hous.release.data.entity.request.LoginRequest
 import hous.release.data.entity.response.BaseResponse
 import hous.release.data.entity.response.LoginResponse
 import hous.release.data.service.AuthService
-import hous.release.domain.entity.request.DomainLoginRequest
 import javax.inject.Inject
 
 class AuthDataSource @Inject constructor(
     private val authService: AuthService
 ) {
     suspend fun postLogin(loginRequest: LoginRequest): BaseResponse<LoginResponse> =
-        authService.postLogin(
-            DomainLoginRequest(
-                fcmToken = loginRequest.fcmToken,
-                socialType = loginRequest.socialType,
-                token = loginRequest.token
-            )
-        )
+        authService.postLogin(loginRequest)
 }
