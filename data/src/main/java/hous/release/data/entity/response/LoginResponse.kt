73dd7043d90848fd722fd9dbd15e2de78ff56191
이빵주 <yungju9982@naver.com>
2022-09-21 22:48:48 +0900

    #14 [fix] LoginResponse token값 TokenEntity 상속

--- data/src/main/java/hous/release/data/entity/response/LoginResponse.kt
+++ data/src/main/java/hous/release/data/entity/response/LoginResponse.kt
@@ -4,5 +4,6 @@ import hous.release.data.entity.TokenEntity
 import hous.release.domain.entity.response.Login
 
 data class LoginResponse(
+    override val token: List<TokenEntity>,
     override val userId: String
 ) : Login
