9a29fd2a744de255a5ef56a5d92b7b0de8cf67b2
이빵주 <yungju9982@naver.com>
2022-09-21 22:45:53 +0900

    #14 [fix] AuthService의 postLogin body값 수정

--- data/src/main/java/hous/release/data/service/AuthService.kt
+++ data/src/main/java/hous/release/data/service/AuthService.kt
@@ -1,14 +1,14 @@
 package hous.release.data.service
 
+import hous.release.data.entity.request.LoginRequest
 import hous.release.data.entity.response.BaseResponse
 import hous.release.data.entity.response.LoginResponse
-import hous.release.domain.entity.request.DomainLoginRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("/v1/auth/login")
     suspend fun postLogin(
-        @Body body: DomainLoginRequest
+        @Body body: LoginRequest
     ): BaseResponse<LoginResponse>
 }
