334d3690d59d3de51f0a229f551c1f923e800fde
이빵주 <yungju9982@naver.com>
2022-09-21 20:52:42 +0900

    #14 [add] providesAuthRepository 추가

--- app/src/main/java/hous/release/android/di/RepositoryModule.kt
+++ app/src/main/java/hous/release/android/di/RepositoryModule.kt
@@ -4,14 +4,22 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hous.release.data.datasource.AuthDataSource
 import hous.release.data.datasource.EnterRoomDataSource
-import hous.release.domain.repository.EnterRoomRepository
+import hous.release.data.repository.AuthRepositoryImpl
 import hous.release.data.repository.EnterRoomRepositoryImpl
+import hous.release.domain.repository.AuthRepository
+import hous.release.domain.repository.EnterRoomRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RepositoryModule {
+    @Provides
+    @Singleton
+    fun providesAuthRepository(authDataSource: AuthDataSource): AuthRepository =
+        AuthRepositoryImpl(authDataSource)
+
     @Provides
     @Singleton
     fun providesEnterRoomRepository(enterRoomDataSource: EnterRoomDataSource): EnterRoomRepository =
