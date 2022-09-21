c12aa6ef212d2c0f21327eb0b670d33ad7cb0e9f
이빵주 <yungju9982@naver.com>
2022-09-21 20:53:00 +0900

    #14 [add] providesAuthService 추가

--- app/src/main/java/hous/release/android/di/RetrofitServiceModule.kt
+++ app/src/main/java/hous/release/android/di/RetrofitServiceModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hous.release.data.service.AuthService
 import hous.release.data.service.EnterRoomService
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -11,6 +12,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class RetrofitServiceModule {
+    @Provides
+    @Singleton
+    fun providesAuthService(retrofit: Retrofit): AuthService =
+        retrofit.create(AuthService::class.java)
+
     @Provides
     @Singleton
     fun providesEnterRoomService(retrofit: Retrofit): EnterRoomService =
