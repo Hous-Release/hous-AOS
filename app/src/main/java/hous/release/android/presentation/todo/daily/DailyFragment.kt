7145f937d232eebceba91839124c857a3d5d50fe
KWY0218 <wodrk1001@gmail.com>
2022-09-22 14:13:03 +0900

    #28 [ui] 요일 별 보기 뷰 형태 구현

---
+++ app/src/main/java/hous/release/android/presentation/todo/daily/DailyFragment.kt
@@ -0,0 +1,7 @@
+package hous.release.android.presentation.todo.daily
+
+import hous.release.android.R
+import hous.release.android.databinding.FragmentDailyBinding
+import hous.release.android.util.binding.BindingFragment
+
+class DailyFragment : BindingFragment<FragmentDailyBinding>(R.layout.fragment_daily)
