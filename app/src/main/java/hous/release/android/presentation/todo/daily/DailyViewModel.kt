81ecca0954815514278bbe3ef47e3666797ef5f8
KWY0218 <wodrk1001@gmail.com>
2022-09-25 18:13:13 +0900

    #28 [chore] 로그 제거

--- app/src/main/java/hous/release/android/presentation/todo/daily/DailyViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/todo/daily/DailyViewModel.kt
@@ -1,16 +1,15 @@
 package hous.release.android.presentation.todo.daily
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hous.release.domain.entity.response.ToDoMain
 import hous.release.domain.usecase.GetDailyToDosUseCase
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltViewModel
 class DailyViewModel @Inject constructor(
@@ -31,11 +30,9 @@ class DailyViewModel @Inject constructor(
             dailyToDosUseCase()
                 .onSuccess { result ->
                     _dailyToDos.value = result
-                    Log.d("sdafkj", "success $result")
                 }
                 .onFailure {
                     Timber.d("error : ${it.message}")
-                    Log.d("sdafkj", "success ${it.message}")
                 }
         }
     }
