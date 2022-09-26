7fb03aa5c0a96c59dc7e15055bf1fa02df9911f4
murjune <sis9221302@gmail.com>
2022-09-26 22:35:35 +0900

    #32 [chore] import문 삭제

--- app/src/main/java/hous/release/android/presentation/our_rules/OurRulesBottomSheetDialogFragment.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/OurRulesBottomSheetDialogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import hous.release.android.R
