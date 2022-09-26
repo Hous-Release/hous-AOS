91b284e6e49d94a8fe4e4d01c387fed233a70bdd
murjune <sis9221302@gmail.com>
2022-09-26 22:24:29 +0900

    #32 [ui] 우리집 추가, 수정, 삭제 기본 뷰 추가

---
+++ app/src/main/java/hous/release/android/presentation/our_rules/OurRuleAddFragment.kt
@@ -0,0 +1,8 @@
+package hous.release.android.presentation.our_rules
+
+import hous.release.android.R
+import hous.release.android.databinding.FragmentOurRuleAddBinding
+import hous.release.android.util.binding.BindingFragment
+
+class OurRuleAddFragment :
+    BindingFragment<FragmentOurRuleAddBinding>(R.layout.fragment_our_rule_add)
