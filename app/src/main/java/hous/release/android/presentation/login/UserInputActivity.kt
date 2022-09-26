09e9b385ac5040aacd22e2b92754aa4af14da528
이빵주 <yungju9982@naver.com>
2022-09-27 05:14:52 +0900

    #5 [add] UserInputActivity에 initIsInputUserInfoObserve 생성

--- app/src/main/java/hous/release/android/presentation/login/UserInputActivity.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputActivity.kt
@@ -6,6 +6,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.ActivityUserInputBinding
 import hous.release.android.util.binding.BindingActivity
+import hous.release.android.util.extension.EventObserver
 
 @AndroidEntryPoint
 class UserInputActivity : BindingActivity<ActivityUserInputBinding>(R.layout.activity_user_input) {
@@ -14,5 +15,17 @@ class UserInputActivity : BindingActivity<ActivityUserInputBinding>(R.layout.act
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = userInputViewModel
+        initIsInputUserInfoObserver()
+    private fun initIsInputUserInfoObserver() {
+        userInputViewModel.isInputUserInfo.observe(
+            this,
+            EventObserver { full ->
+                if (full) {
+                    initChangeBtnNextColor()
+                    initBtnNextOnClickListener()
+                }
+            }
+        )
+    }
     }
 }
