181653540901b569f6134ae019f4429f501551f0
이빵주 <yungju9982@naver.com>
2022-09-27 05:12:04 +0900

    #5 [add] UserInputViewModel에 _isInputUserInfo 추가

--- app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
@@ -1,7 +1,9 @@
 package hous.release.android.presentation.login
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import hous.release.android.util.extension.Event
 
 class UserInputViewModel : ViewModel() {
     private val _nickname = MutableLiveData<String>()
@@ -9,4 +11,15 @@ class UserInputViewModel : ViewModel() {
 
     private val _birthday = MutableLiveData<String>()
     val birthday get() = _birthday
+
+    private val _isInputUserInfo = MediatorLiveData<Event<Boolean>>().apply {
+        addSource(nickname) { nickname ->
+            value = Event(nickname.isNotBlank() && birthday.value != null)
+        }
+        addSource(birthday) { birthday ->
+            value = Event(birthday.isNotBlank() && nickname.value != null)
+        }
+    }
+    val isInputUserInfo get() = _isInputUserInfo
+
 }
