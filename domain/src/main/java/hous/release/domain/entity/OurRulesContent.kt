956f59d40f61a695e806f7b595279577ce7edd95
murjune <sis9221302@gmail.com>
2022-09-28 21:23:21 +0900

    #32 [fix] interface -> data class로 변경 및 mapper 함수 추가

--- domain/src/main/java/hous/release/domain/entity/OurRulesContent.kt
+++ domain/src/main/java/hous/release/domain/entity/OurRulesContent.kt
@@ -2,9 +2,17 @@ package hous.release.domain.entity
 
 import hous.release.domain.entity.response.OurRule
 
-interface OurRulesContent {
-    val ourRuleList: List<OurRule>
-    val isEmptyRepresentativeRuleList: Boolean
-    val isEmptyGeneralRuleList: Boolean
-    val errorState: Boolean
+data class OurRulesContent(
+    val ourRuleList: List<OurRule> = defaultRuleList,
+    val isEmptyRepresentativeRuleList: Boolean = true,
+    val isEmptyGeneralRuleList: Boolean = true,
+    val errorMsg: String? = ""
+) {
+    companion object {
+        val defaultRuleList = listOf(
+            OurRule(),
+            OurRule(),
+            OurRule()
+        )
+    }
 }
