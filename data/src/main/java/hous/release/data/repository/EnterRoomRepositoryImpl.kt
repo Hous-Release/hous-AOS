bda355693349f8fe183c28bd145b2e27f85adcc1
Yunjoo Son <thsduswn4141@naver.com>
2022-09-29 14:50:13 +0900

    #40 [feat] 방 입장하기 부분 repository return 방식 수정

--- data/src/main/java/hous/release/data/repository/EnterRoomRepositoryImpl.kt
+++ data/src/main/java/hous/release/data/repository/EnterRoomRepositoryImpl.kt
@@ -12,52 +12,30 @@ import javax.inject.Inject
 class EnterRoomRepositoryImpl @Inject constructor(
     private val enterRoomDataSource: EnterRoomDataSource
 ) : EnterRoomRepository {
-    override suspend fun postCreateRoom(createRoomRequest: DomainCreateRoomRequest): Result<DomainCreateRoomResponse> {
+    override suspend fun postCreateRoom(createRoomRequest: DomainCreateRoomRequest): Result<DomainCreateRoomResponse> =
         kotlin.runCatching { enterRoomDataSource.postCreateRoom(CreateRoomRequest(name = createRoomRequest.name)) }
-            .onSuccess { response ->
-                return Result.success(
-                    DomainCreateRoomResponse(
-                        roomCode = response.data.roomCode,
-                        roomId = response.data.roomId
-                    )
+            .map { response ->
+                DomainCreateRoomResponse(
+                    roomCode = response.data.roomCode,
+                    roomId = response.data.roomId
                 )
             }
-            .onFailure { throw it }
 
-        throw IllegalStateException(UNKNOWN_ERROR)
-    }
-
-    override suspend fun getEnterRoomCode(roomCode: String): Result<DomainGetRoomResponse> {
+    override suspend fun getEnterRoomCode(roomCode: String): Result<DomainGetRoomResponse> =
         kotlin.runCatching { enterRoomDataSource.getEnterRoomCode(roomCode) }
-            .onSuccess { response ->
-                return Result.success(
-                    DomainGetRoomResponse(
-                        nickname = response.data.nickname,
-                        roomId = response.data.roomId
-                    )
+            .map { response ->
+                DomainGetRoomResponse(
+                    nickname = response.data.nickname,
+                    roomId = response.data.roomId
                 )
             }
-            .onFailure { throw it }
 
-        throw IllegalStateException(UNKNOWN_ERROR)
-    }
-
-    override suspend fun postEnterRoomId(roomId: String): Result<DomainEnterRoomResponse> {
+    override suspend fun postEnterRoomId(roomId: String): Result<DomainEnterRoomResponse> =
         kotlin.runCatching { enterRoomDataSource.postEnterRoomId(roomId) }
-            .onSuccess { response ->
-                return Result.success(
-                    DomainEnterRoomResponse(
-                        roomCode = response.data.roomCode,
-                        roomId = response.data.roomId
-                    )
+            .map { response ->
+                DomainEnterRoomResponse(
+                    roomCode = response.data.roomCode,
+                    roomId = response.data.roomId
                 )
             }
-            .onFailure { throw it }
-
-        throw IllegalStateException(UNKNOWN_ERROR)
-    }
-
-    companion object {
-        const val UNKNOWN_ERROR = "네트워크 통신 중 알 수 없는 오류"
-    }
 }
