8e49003d29fb19cb5373392961574ceda3c21552
murjune <sis9221302@gmail.com>
2022-09-30 21:01:29 +0900

    #41 [feat] 우리집 규칙 추가뷰 로직 구현 완

--- app/src/main/java/hous/release/android/presentation/our_rules/add_rule/OurRuleAddFragment.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/add_rule/OurRuleAddFragment.kt
@@ -2,21 +2,99 @@ package hous.release.android.presentation.our_rules.add_rule
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentOurRuleAddBinding
+import hous.release.android.presentation.our_rules.adapter.OurRulesAddAdapter
+import hous.release.android.presentation.our_rules.type.SaveButtonState
 import hous.release.android.util.binding.BindingFragment
-import hous.release.android.util.showToast
+import hous.release.android.util.extension.repeatOnStarted
+import hous.release.android.util.extension.safeLet
+import timber.log.Timber
 
+@AndroidEntryPoint
 class OurRuleAddFragment :
     BindingFragment<FragmentOurRuleAddBinding>(R.layout.fragment_our_rule_add) {
+
+    private val viewModel by viewModels<OurRuleAddViewModel>()
+    private lateinit var ourRulesAddAdapter: OurRulesAddAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.ivAddRulePlusBtn.setOnClickListener {
-            context?.showToast("버튼 누름")
+        binding.vm = viewModel
+        initBackButtonListener()
+        initSaveButtonListener()
+        initAddRuleButtonListener()
+        initAdapter()
+        observeUiState()
+    }
+
+    private fun observeUiState() {
+        repeatOnStarted {
+            viewModel.uiState.collect { uiState ->
+                ourRulesAddAdapter.submitList(uiState.ourRuleList)
+                if (viewModel.uiState.value.addedRuleList.isNotEmpty()) {
+                    viewModel.setSaveButtonState(SaveButtonState.ACTIVE)
+                } else {
+                    viewModel.setSaveButtonState(SaveButtonState.INACTIVE)
+                }
+            }
         }
-        binding.ivAddRuleBackButton.setOnClickListener {
+    }
+
+    private fun initAdapter() {
+        ourRulesAddAdapter = OurRulesAddAdapter()
+        binding.rvAddOurRules.run {
+            adapter = ourRulesAddAdapter
+            itemAnimator = null
+        }
+    }
+
+    private fun initAddRuleButtonListener() {
+        binding.ivAddRuleBtn.setOnClickListener {
+            if (viewModel.uiState.value.ourRuleList.size > 30) {
+                val errorDialogFragment = OurRuleAddErrorDialogFragment()
+                errorDialogFragment.show(parentFragmentManager, OUR_RULE_ADD_ERROR_DIALOG)
+                return@setOnClickListener
+            }
+            viewModel.addRule()
+        }
+    }
+
+    private fun initSaveButtonListener() {
+        binding.ivAddRuleSaveButton.setOnClickListener {
+            viewModel.putAddRuleList()
             findNavController().popBackStack()
         }
+    }
+
+    private fun initBackButtonListener() {
+        safeLet(activity, activity?.onBackPressedDispatcher) { _, dispatcher ->
+            dispatcher.addCallback {
+                if (viewModel.uiState.value.saveButtonState == SaveButtonState.ACTIVE || viewModel.inputRuleNameField.value.isNotBlank()) {
+                    val outDialogFragment = OurRuleAddOutDialogFragment()
+                    outDialogFragment.show(parentFragmentManager, OUR_RULE_ADD_OUT_DIALOG)
+                    return@addCallback
+                }
+                findNavController().popBackStack()
+            }
+        } ?: Timber.e(getString(R.string.null_point_exception))
+
+        binding.ivAddRuleBackButton.setOnClickListener {
+            if (viewModel.uiState.value.saveButtonState == SaveButtonState.ACTIVE || viewModel.inputRuleNameField.value.isNotBlank()) {
+                val outDialogFragment = OurRuleAddOutDialogFragment()
+                outDialogFragment.show(parentFragmentManager, OUR_RULE_ADD_OUT_DIALOG)
+                return@setOnClickListener
+            }
+            findNavController().popBackStack()
+        }
+    }
+
+    companion object {
+        const val OUR_RULE_ADD_ERROR_DIALOG = "our_rule_add_error_dialog"
+        const val OUR_RULE_ADD_OUT_DIALOG = "our_rule_add_out_dialog"
     }
 }
---
+++ app/src/main/java/hous/release/android/presentation/our_rules/add_rule/OurRuleAddViewModel.kt
@@ -0,0 +1,98 @@
+package hous.release.android.presentation.our_rules.add_rule
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hous.release.android.presentation.our_rules.type.SaveButtonState
+import hous.release.domain.entity.ApiResult
+import hous.release.domain.entity.response.OurRule
+import hous.release.domain.usecase.FetchOurRulesUseCase
+import hous.release.domain.usecase.PutAddRulesUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class OurRuleAddViewModel @Inject constructor(
+    private val ourRulesUseCase: FetchOurRulesUseCase,
+    private val putAddRulesUseCase: PutAddRulesUseCase
+) :
+    ViewModel() {
+    private var tmpId = -1 // 임시로 저장할 id가 필요 -- 서버야 id값 음수로는 안보내지는지 물어봐야함
+    var inputRuleNameField = MutableStateFlow<String>("")
+    private var _uiState = MutableStateFlow(OurRuleAddUIState())
+    val uiState: StateFlow<OurRuleAddUIState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            ourRulesUseCase.fetchOurRules().collect { apiResult ->
+                when (apiResult) {
+                    is ApiResult.Success ->
+                        _uiState.value =
+                            _uiState.value.copy(isLoading = false, ourRuleList = apiResult.data)
+                    is ApiResult.Empty -> {
+                        // TODO EMPTY 뷰 나오면 ERROR 로직 처리해주기
+                        _uiState.value = _uiState.value.copy(isEmpty = true, isLoading = false)
+                    }
+                    is ApiResult.Error -> {
+                        // TODO ERROR 뷰 나오면 ERROR 로직 처리해주기
+                        _uiState.value = _uiState.value.copy(isError = true, isLoading = false)
+                    }
+                }
+            }
+        }
+    }
+
+    fun initInputRuleNameField() {
+        inputRuleNameField.value = ""
+    }
+
+    fun setSaveButtonState(saveButtonState: SaveButtonState) {
+        Timber.i("saveButtonState is active? ${saveButtonState == SaveButtonState.ACTIVE}")
+        _uiState.value = uiState.value.copy(
+            saveButtonState = saveButtonState
+        )
+    }
+
+    fun addRule() {
+        if (inputRuleNameField.value.isNotBlank()) {
+            _uiState.value =
+                uiState.value.copy(
+                    ourRuleList = _uiState.value.ourRuleList + listOf(
+                        OurRule(
+                            tmpId--,
+                            inputRuleNameField.value
+                        )
+                    ),
+                    addedRuleList = _uiState.value.addedRuleList + listOf(
+                        inputRuleNameField.value
+                    )
+                )
+            initInputRuleNameField()
+        }
+    }
+
+    fun putAddRuleList() {
+        viewModelScope.launch {
+            putAddRulesUseCase.putAddRule(uiState.value.addedRuleList).collect { apiResult ->
+                when (apiResult) {
+                    is ApiResult.Success -> Timber.i("putAddRuleList success")
+                    is ApiResult.Error -> Timber.e("putAddRuleList error -${apiResult.throwable}")
+                    is ApiResult.Empty -> Timber.e("IllegalArgument Exception")
+                }
+            }
+        }
+    }
+
+    data class OurRuleAddUIState(
+        val ourRuleList: List<OurRule> = emptyList(),
+        val addedRuleList: List<String> = emptyList(),
+        val isError: Boolean = false,
+        val isEmpty: Boolean = false,
+        val isLoading: Boolean = true,
+        val saveButtonState: SaveButtonState = SaveButtonState.INACTIVE
+    )
+}
