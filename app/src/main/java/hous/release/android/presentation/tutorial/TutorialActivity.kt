ad519eb11bdf5bb96bf09d7ccb18d4ccdb9302a5
이빵주 <yungju9982@naver.com>
2022-10-04 04:01:30 +0900

    #35 [add] TutorialActivity에 import 추가

--- app/src/main/java/hous/release/android/presentation/tutorial/TutorialActivity.kt
+++ app/src/main/java/hous/release/android/presentation/tutorial/TutorialActivity.kt
@@ -1,8 +1,15 @@
 package hous.release.android.presentation.tutorial
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import hous.release.android.R
+import hous.release.android.databinding.ActivityTutorialBinding
+import hous.release.android.presentation.tutorial.adapter.TutorialAdapter
+import hous.release.android.util.binding.BindingActivity
+import hous.release.domain.entity.TutorialEntity
+
 
 class TutorialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
