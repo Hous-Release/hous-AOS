013f7ae0853be0caf5ad1605618fdb1d259224f2
이빵주 <yungju9982@naver.com>
2022-10-05 03:13:00 +0900

    #5 [fix] UserInput 뷰모델 자료형 수정

--- app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
@@ -1,17 +1,14 @@
 package hous.release.android.presentation.login
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import hous.release.android.util.extension.Event
 
 class UserInputViewModel : ViewModel() {
-    private val _nickname = MutableLiveData<String>()
-    val nickname: LiveData<String> = _nickname
+    val nickname = MutableLiveData<String>()
 
-    private val _birthday = MutableLiveData<String>()
-    val birthday: LiveData<String> = _birthday
+    val birthday = MutableLiveData<String>("1999-08-02")
 
     private val _isInputUserInfo = MediatorLiveData<Event<Boolean>>().apply {
         addSource(nickname) { nickname ->
