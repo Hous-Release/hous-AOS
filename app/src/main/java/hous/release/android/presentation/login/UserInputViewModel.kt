ec46a5f03b86e598c23817cc7625c800dabef8d7
이빵주 <yungju9982@naver.com>
2022-10-07 20:37:39 +0900

    #5 [delete] UserInputViewModel 불필요한 변수 제거

--- app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
@@ -1,24 +1,12 @@
 package hous.release.android.presentation.login
 
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import hous.release.android.util.extension.Event
 
 class UserInputViewModel : ViewModel() {
     val nickname = MutableLiveData<String>()
 
     val birthday = MutableLiveData<String>("1999-08-02")
-
-    private val _isInputUserInfo = MediatorLiveData<Event<Boolean>>().apply {
-        addSource(nickname) { nickname ->
-            value = Event(nickname.isNotBlank() && birthday.value != null)
-        }
-        addSource(birthday) { birthday ->
-            value = Event(birthday.isNotBlank() && nickname.value != null)
-        }
-    }
-    val isInputUserInfo get() = _isInputUserInfo
 
     private val _isBtnCheckBirthday = MutableLiveData(false)
     val isBtnCheckBirthday get() = _isBtnCheckBirthday
