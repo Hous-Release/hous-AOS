26429303ae1276356083155a45e41532c21dd8f9
이빵주 <yungju9982@naver.com>
2022-10-07 19:01:16 +0900

    #35 [fix] TutorialDataSource 패키지 및 함수명 변경

--- data/src/main/java/hous/release/data/datasource/TutorialDataSource.kt
+++ data/src/main/java/hous/release/data/datasource/TutorialDataSource.kt
@@ -1,26 +1,24 @@
-package hous.release.android.util
+package hous.release.data.datasource
 
 import android.content.Context
 import android.content.SharedPreferences
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class SharedPreference @Inject constructor(@ApplicationContext context: Context) {
+class TutorialDataSource @Inject constructor(@ApplicationContext context: Context) {
     private val prefs: SharedPreferences =
         context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
 
-    fun setShowTutorial(showTutorial: Boolean) {
-        prefs.edit().putBoolean(SHOW_TUTORIAL, showTutorial).apply()
+    fun setShowTutorial(skipTutorial: Boolean) {
+        prefs.edit().putBoolean(SKIP_TUTORIAL, skipTutorial).apply()
     }
 
     fun getShowTutorial(): Boolean {
-        return prefs.getBoolean(SHOW_TUTORIAL, false)
+        return prefs.getBoolean(SKIP_TUTORIAL, false)
     }
 
     companion object {
-        private const val SHOW_TUTORIAL = "ShowTutorial"
+        private const val SKIP_TUTORIAL = "SkipTutorial"
         private const val STORAGE_KEY = "StorageKey"
     }
 }
