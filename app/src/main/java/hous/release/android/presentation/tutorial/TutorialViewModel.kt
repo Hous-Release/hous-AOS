b47d49171860fe13d610cd5b0be76e7b813bea1b
이빵주 <yungju9982@naver.com>
2022-10-07 19:02:41 +0900

    #35 [add] TutorialViewModel에 next버튼 클릭시 앞으로는 스킵되는 로직 구현

--- app/src/main/java/hous/release/android/presentation/tutorial/TutorialViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/tutorial/TutorialViewModel.kt
@@ -1,8 +1,32 @@
 package hous.release.android.presentation.tutorial
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import hous.release.data.datasource.TutorialDataSource
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TutorialViewModel : ViewModel() {
+class TutorialViewModel @Inject constructor(
+    private val tutorialDataSource: TutorialDataSource
+) : ViewModel() {
     val showNextBtn = MutableLiveData<Boolean>()
+
+    private val _isSkipTutorial = MutableLiveData<Boolean>()
+    val skipTutorial: LiveData<Boolean> = _isSkipTutorial
+
+    init {
+        viewModelScope.launch {
+            if (tutorialDataSource.getShowTutorial()) {
+                _isSkipTutorial.value = true
+            }
+        }
+    }
+
+    fun nextOnClick() {
+        viewModelScope.launch {
+            tutorialDataSource.setShowTutorial(skipTutorial = true)
+        }
+    }
 }
