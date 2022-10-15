e89fad5b2b19c8412aea9294ed3b5fb591d3ac91
murjune <sis9221302@gmail.com>
2022-10-16 04:42:48 +0900

    [feat] DialogFragment initLayout Extension 추가

---
+++ app/src/main/java/hous/release/android/util/extension/DialogFragmentExtension.kt
@@ -0,0 +1,22 @@
+package hous.release.android.util.extension
+
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import hous.release.android.R
+import timber.log.Timber
+
+fun DialogFragment.initLayout() {
+    safeLet(dialog, requireNotNull(dialog).window) { dialog, window ->
+        window.apply {
+            isCancelable = false
+            setBackgroundDrawableResource(R.drawable.shape_white_fill_8_rect)
+        }
+    } ?: Timber.e(
+        getString(R.string.null_point_exception_detail_two_item).format(
+            "dialog",
+            dialog == null,
+            "window",
+            dialog!!.window == null
+        )
+    )
+}
