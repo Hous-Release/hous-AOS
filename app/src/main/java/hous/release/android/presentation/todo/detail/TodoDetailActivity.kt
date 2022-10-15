34f323595d0db03bb6ddc9b8f94e85a64a8076ca
KWY0218 <wodrk1001@gmail.com>
2022-10-15 15:39:28 +0900

    #50 [feat] 플로팅 버튼 추가

--- app/src/main/java/hous/release/android/presentation/todo/detail/TodoDetailActivity.kt
+++ app/src/main/java/hous/release/android/presentation/todo/detail/TodoDetailActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import hous.release.android.R
+import hous.release.android.databinding.ActivityTodoDetailBinding
 import hous.release.android.util.HousFloatingButton
 
 class TodoDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTodoDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo_detail)
+        binding = ActivityTodoDetailBinding.inflate(layoutInflater)
+            .also { binding -> setContentView(binding.root) }
         initStatusBarColor()
+        initFloatingButton()
     }
 
     private fun initStatusBarColor() {
@@ -24,10 +28,10 @@ class TodoDetailActivity : AppCompatActivity() {
     }
 
     private fun initFloatingButton() {
-//        binding.cvMemberFloatingButton.setContent {
-//            HousFloatingButton {
-//                /* TO DO 추가하기 뷰로 이동하는 함수 */
-//            }
-//        }
+        binding.cvMemberFloatingButton.setContent {
+            HousFloatingButton {
+                /* TO DO 추가하기 뷰로 이동하는 함수 */
+            }
+        }
     }
 }
