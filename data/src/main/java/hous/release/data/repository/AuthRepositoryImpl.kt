49ad43538381532eec998a694f21c1dc361721f2
이빵주 <yungju9982@naver.com>
2022-10-17 02:18:51 +0900

    #5 [add] AuthRepositoryImpl에 override 생성

--- data/src/main/java/hous/release/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/hous/release/data/repository/AuthRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package hous.release.data.repository
 
 import hous.release.data.datasource.AuthDataSource
+import hous.release.data.datasource.SharedPrefDataSource
 import hous.release.data.entity.request.LoginRequest
 import hous.release.data.entity.response.LoginResponse
 import hous.release.domain.entity.response.Login
@@ -9,7 +10,8 @@ import java.lang.IllegalStateException
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val authDataSource: AuthDataSource
+    private val authDataSource: AuthDataSource,
+    private val sharedPrefDataSource: SharedPrefDataSource
 ) : AuthRepository {
     override suspend fun postLogin(
         fcmToken: String,
@@ -38,9 +40,18 @@ class AuthRepositoryImpl @Inject constructor(
         throw IllegalStateException(UNKNOWN_ERROR)
     }
 
+    override suspend fun initShowTutorial(skipTutorial: Boolean) {
+        sharedPrefDataSource.prefs.edit().putBoolean(SKIP_TUTORIAL, skipTutorial).apply()
+    }
+
+    override suspend fun getShowTutorial(): Boolean {
+        return sharedPrefDataSource.prefs.getBoolean(SKIP_TUTORIAL, false)
+    }
+
     companion object {
         const val UNKNOWN_ERROR = "네트워크 통신 중 알 수 없는 오류"
         const val REFRESH_TOKEN = 0
         const val ACCESS_TOKEN = 1
+        const val SKIP_TUTORIAL = "SkipTutorial"
     }
 }
