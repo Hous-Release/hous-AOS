32adf21c69105f8504d7d0040ce923078d530c60
이빵주 <yungju9982@naver.com>
2022-10-17 02:17:06 +0900

    #5 [add] AuthRepository에 initShowTutorial, getShowTutorial 함수 생성

--- domain/src/main/java/hous/release/domain/repository/AuthRepository.kt
+++ domain/src/main/java/hous/release/domain/repository/AuthRepository.kt
@@ -4,4 +4,8 @@ import hous.release.domain.entity.response.Login
 
 interface AuthRepository {
     suspend fun postLogin(fcmToken: String, socialType: String, token: String): Result<Login>
+
+    suspend fun initShowTutorial(skipTutorial: Boolean)
+
+    suspend fun getShowTutorial(): Boolean
 }
