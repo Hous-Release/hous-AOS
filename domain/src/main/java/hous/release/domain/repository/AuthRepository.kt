928741f67688e3b35c67c202dec55907794b79f7
이빵주 <yungju9982@naver.com>
2022-10-17 01:06:20 +0900

    #48 [fix] AuthRepository의 postLogin 파라미터 변경

--- domain/src/main/java/hous/release/domain/repository/AuthRepository.kt
+++ domain/src/main/java/hous/release/domain/repository/AuthRepository.kt
@@ -1,7 +1,8 @@
 package hous.release.domain.repository
 
-import hous.release.domain.entity.response.Login
+import hous.release.domain.entity.request.DomainLoginRequest
+import hous.release.domain.entity.response.DomainLoginResponse
 
 interface AuthRepository {
-    suspend fun postLogin(fcmToken: String, socialType: String, token: String): Result<Login>
+    suspend fun postLogin(loginRequest: DomainLoginRequest): Result<DomainLoginResponse>
 }
