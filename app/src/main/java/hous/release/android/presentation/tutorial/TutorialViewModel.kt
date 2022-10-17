af8b50aa373bbd871383557761929a4872749157
이빵주 <yungju9982@naver.com>
2022-10-18 03:00:19 +0900

    #5 [fix] TutorialViewModel에 initSkipTutorial 수정

--- app/src/main/java/hous/release/android/presentation/tutorial/TutorialViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/tutorial/TutorialViewModel.kt
@@ -5,30 +5,22 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import hous.release.domain.usecase.GetSkipTutorialUseCase
 import hous.release.domain.usecase.InitSkipTutorialUseCase
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TutorialViewModel @Inject constructor(
-    private val getSkipTutorialUseCase: GetSkipTutorialUseCase,
     private val initSkipTutorialUseCase: InitSkipTutorialUseCase
 ) : ViewModel() {
     val showNextBtn = MutableLiveData<Boolean>()
 
-    private val _isTutorialState = MutableLiveData<Boolean>()
+    private val _isTutorialState = MutableLiveData<Boolean>(false)
     val isTutorialState: LiveData<Boolean> = _isTutorialState
-
-    init {
-        viewModelScope.launch {
-            _isTutorialState.value = getSkipTutorialUseCase.invoke()
-        }
-    }
 
     fun nextOnClick() {
         viewModelScope.launch {
-            initSkipTutorialUseCase.invoke(skipTutorial = true)
+            initSkipTutorialUseCase(skipTutorial = true)
             _isTutorialState.value = true
         }
     }
