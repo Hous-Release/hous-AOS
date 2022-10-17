3c700478a68e4cb492c227cb0367b5677e7bf1c2
이빵주 <yungju9982@naver.com>
2022-10-18 04:09:12 +0900

    #5 [add] 백버튼 두 번 클릭시 종료되는 initBackPressedCallBack 함수 생성

--- app/src/main/java/hous/release/android/presentation/login/UserInputActivity.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputActivity.kt
@@ -2,16 +2,20 @@ package hous.release.android.presentation.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.ActivityUserInputBinding
 import hous.release.android.presentation.enter_room.EnterRoomActivity
 import hous.release.android.util.binding.BindingActivity
+import hous.release.android.util.showToast
+import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class UserInputActivity : BindingActivity<ActivityUserInputBinding>(R.layout.activity_user_input) {
     private val userInputViewModel: UserInputViewModel by viewModels()
+    private var onBackPressedTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,25 @@ class UserInputActivity : BindingActivity<ActivityUserInputBinding>(R.layout.act
         initNextBtnOnClickListener()
         initBackBtnOnClickListener()
         initCheckBirthdayBtnOnClickListener()
+        initBackPressedCallback()
+    }
+
+    private fun initBackPressedCallback() {
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (System.currentTimeMillis() - onBackPressedTime >= WAITING_DEADLINE) {
+                        onBackPressedTime = System.currentTimeMillis()
+                        showToast(getString(R.string.finish_app_toast_msg))
+                    } else {
+                        finishAffinity()
+                        System.runFinalization()
+                        exitProcess(0)
+                    }
+                }
+            }
+        )
     }
 
     private fun initBackBtnOnClickListener() {
@@ -39,5 +62,9 @@ class UserInputActivity : BindingActivity<ActivityUserInputBinding>(R.layout.act
             startActivity(toEnterRoom)
             finishAffinity()
         }
+    }
+
+    companion object {
+        private const val WAITING_DEADLINE = 2000L
     }
 }
