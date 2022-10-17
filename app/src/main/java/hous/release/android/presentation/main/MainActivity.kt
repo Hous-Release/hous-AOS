60fc5e78e09f7362460326ae5186433610204830
murjune <sis9221302@gmail.com>
2022-10-17 15:58:14 +0900

    [feat] 뒤로 가기 2번 클릭시 앱종료 로직 구현

--- app/src/main/java/hous/release/android/presentation/main/MainActivity.kt
+++ app/src/main/java/hous/release/android/presentation/main/MainActivity.kt
@@ -1,19 +1,26 @@
 package hous.release.android.presentation.main
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.ActivityMainBinding
 import hous.release.android.util.binding.BindingActivity
+import hous.release.android.util.extension.showToast
+import timber.log.Timber
+import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private var onBackPressedTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBottomNavigationView()
+        initBackPressedCallback()
     }
 
     private fun initBottomNavigationView() {
@@ -25,5 +32,29 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
         return navHostFragment.navController
+    }
+
+    private fun initBackPressedCallback() {
+        onBackPressedDispatcher.addCallback {
+            val fragmentId: Int? = findNavController(R.id.fc_main).currentDestination?.id
+            fragmentId?.also { id ->
+                if (id == R.id.housFragment) {
+                    val curTime = System.currentTimeMillis()
+                    val gap = curTime - onBackPressedTime
+                    if (gap > WAITING_DEADLINE) {
+                        onBackPressedTime = curTime
+                        showToast(getString(R.string.finish_app_toast_msg))
+                        return@addCallback
+                    }
+                    finishAffinity()
+                    System.runFinalization()
+                    exitProcess(0)
+                }
+            } ?: Timber.e(getString(R.string.null_point_exception))
+        }
+    }
+
+    companion object {
+        private const val WAITING_DEADLINE = 2000L
     }
 }
