d33d76737d8e8cda97cdc2498376bcd4160325a0
murjune <sis9221302@gmail.com>
2022-10-19 20:57:34 +0900

    #55 [feat] 규칙 수정 프래그먼트 뷰모델 기초세팅

--- app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditFragment.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditFragment.kt
@@ -1,8 +1,10 @@
 package hous.release.android.presentation.our_rules.edit_rule
 
+import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentOurRuleEditBinding
 import hous.release.android.util.binding.BindingFragment
 
+@AndroidEntryPoint
 class OurRuleEditFragment :
     BindingFragment<FragmentOurRuleEditBinding>(R.layout.fragment_our_rule_edit)
