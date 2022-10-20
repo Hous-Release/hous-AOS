a7db7ca5f3f92098d86a52877b5849d65c12c1e4
이빵주 <yungju9982@naver.com>
2022-10-21 04:19:08 +0900

    #62 [add] AuthRepositoryImpl에 postSignUp 생성

--- data/src/main/java/hous/release/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/hous/release/data/repository/AuthRepositoryImpl.kt
@@ -3,6 +3,7 @@ package hous.release.data.repository
 import hous.release.data.datasource.AuthDataSource
 import hous.release.data.datasource.LocalPrefSkipTutorialDataSource
 import hous.release.domain.entity.response.Login
+import hous.release.domain.entity.response.SignUp
 import hous.release.domain.repository.AuthRepository
 import javax.inject.Inject
 
@@ -22,6 +23,25 @@ class AuthRepositoryImpl @Inject constructor(
                 token
             )
         }.map { response -> response.data.toLogin() }
+
+    override suspend fun postSignUp(
+        birthday: String,
+        fcmToken: String,
+        isPublic: Boolean,
+        nickname: String,
+        socialType: String,
+        token: String
+    ): Result<SignUp> =
+        kotlin.runCatching {
+            authDataSource.postSignUp(
+                birthday,
+                fcmToken,
+                isPublic,
+                nickname,
+                socialType,
+                token
+            )
+        }.map { response -> response.data.toSignUp() }
 
     override suspend fun initSkipTutorial(skipTutorial: Boolean) {
         localPrefSkipTutorialDataSource.showTutorial = skipTutorial
