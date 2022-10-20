40741516d1bac9c8a4bb2ab3570e8c5b12a06fe4
이빵주 <yungju9982@naver.com>
2022-10-21 04:18:48 +0900

    #62 [add] AuthDataSource에 postSignUp 생성

--- data/src/main/java/hous/release/data/datasource/AuthDataSource.kt
+++ data/src/main/java/hous/release/data/datasource/AuthDataSource.kt
@@ -1,8 +1,10 @@
 package hous.release.data.datasource
 
 import hous.release.data.entity.request.LoginRequest
+import hous.release.data.entity.request.SignUpRequest
 import hous.release.data.entity.response.BaseResponse
 import hous.release.data.entity.response.LoginResponse
+import hous.release.data.entity.response.SignUpResponse
 import hous.release.data.service.AuthService
 import javax.inject.Inject
 
@@ -15,4 +17,23 @@ class AuthDataSource @Inject constructor(
         token: String
     ): BaseResponse<LoginResponse> =
         authService.postLogin(LoginRequest(fcmToken, socialType, token))
+
+    suspend fun postSignUp(
+        birthday: String,
+        fcmToken: String,
+        isPublic: Boolean,
+        nickname: String,
+        socialType: String,
+        token: String
+    ): BaseResponse<SignUpResponse> =
+        authService.postSignUp(
+            SignUpRequest(
+                birthday,
+                fcmToken,
+                isPublic,
+                nickname,
+                socialType,
+                token
+            )
+        )
 }
