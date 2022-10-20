3d1e54064fa1e24445428b5a49bd46a2df710c07
이빵주 <yungju9982@naver.com>
2022-10-21 04:34:12 +0900

    #62 [add] UserInputViewModel에 authRepository 주입

--- app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
@@ -2,8 +2,14 @@ package hous.release.android.presentation.login
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hous.release.domain.repository.AuthRepository
 
 class UserInputViewModel : ViewModel() {
+@HiltViewModel
+class UserInputViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
     val nickname = MutableLiveData<String>()
 
     val birthday = MutableLiveData<String>("1999-08-02")
