773a270d438bd9a7869b5b7c569b42e67b25d00f
이빵주 <yungju9982@naver.com>
2022-10-21 04:34:55 +0900

    #62 [add] UserInput 다음으로 버튼 클릭시 서버연결

--- app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/login/UserInputViewModel.kt
@@ -1,11 +1,15 @@
 package hous.release.android.presentation.login
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hous.release.domain.repository.AuthRepository
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class UserInputViewModel : ViewModel() {
 @HiltViewModel
 class UserInputViewModel @Inject constructor(
     private val authRepository: AuthRepository
@@ -15,4 +19,24 @@ class UserInputViewModel @Inject constructor(
     val birthday = MutableLiveData<String>("1999-08-02")
 
     val isCheckBirthday = MutableLiveData<Boolean>()
+
+    private val _isSignUp = MutableLiveData<Boolean>()
+    val isSignUp: LiveData<Boolean> = _isSignUp
+
+    fun onClickNextBtn() {
+        viewModelScope.launch {
+            authRepository.postSignUp(
+                birthday = birthday.value!!,
+                fcmToken =,
+                isPublic = isCheckBirthday.value!!,
+                nickname = nickname.value!!,
+                socialType =,
+                token =
+            ).onSuccess {
+                _isSignUp.value = true
+            }.onFailure {
+                _isSignUp.value = false
+            }
+        }
+    }
 }
