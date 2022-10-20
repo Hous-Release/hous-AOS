db704b9077b16b85df22567c0608e541d78040da
이빵주 <yungju9982@naver.com>
2022-10-21 01:07:22 +0900

    #48 [delete] Login에서 status 변수 제거

--- domain/src/main/java/hous/release/domain/entity/response/Login.kt
+++ domain/src/main/java/hous/release/domain/entity/response/Login.kt
@@ -1,7 +1,6 @@
 package hous.release.domain.entity.response
 
 data class Login(
-    val status: Int = 0,
     val isJoiningRoom: Boolean = false,
     val token: Token = Token(),
     val userId: String = ""
