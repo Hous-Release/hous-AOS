c51d885899178c7104b53f58ad24635bdfa58e30
Yunjoo Son <thsduswn4141@naver.com>
2022-10-20 18:18:04 +0900

    #57 [chore] 다크모드 막기

---
+++ app/src/main/java/hous/release/android/presentation/hous/EditHousNameActivity.kt
@@ -0,0 +1,13 @@
+package hous.release.android.presentation.hous
+
+import android.os.Bundle
+import hous.release.android.R
+import hous.release.android.databinding.ActivityEditHousNameBinding
+import hous.release.android.util.binding.BindingActivity
+
+class EditHousNameActivity :
+    BindingActivity<ActivityEditHousNameBinding>(R.layout.activity_edit_hous_name) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
