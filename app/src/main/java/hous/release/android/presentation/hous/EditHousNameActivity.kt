e5e215320447d837c152c67c8433e4e42c77827e
Yunjoo Son <thsduswn4141@naver.com>
2022-10-21 16:25:52 +0900

    #57 [feat] 하우스명 수정 화면 이동, 하우스명 수정 시 경고 다이얼로그 동작 설정

--- app/src/main/java/hous/release/android/presentation/hous/EditHousNameActivity.kt
+++ app/src/main/java/hous/release/android/presentation/hous/EditHousNameActivity.kt
@@ -6,8 +6,15 @@ import hous.release.android.databinding.ActivityEditHousNameBinding
 import hous.release.android.util.binding.BindingActivity
 
 class EditHousNameActivity :
-    BindingActivity<ActivityEditHousNameBinding>(R.layout.activity_edit_hous_name) {
+        BindingActivity<ActivityEditHousNameBinding>(R.layout.activity_edit_hous_name) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.btnEditHousNameBack.setOnClickListener {
+            WarningDialogFragment().show(supportFragmentManager, "경고")
+        }
     }
 }
--- app/src/main/java/hous/release/android/presentation/hous/WarningDialogFragment.kt
+++ app/src/main/java/hous/release/android/presentation/hous/WarningDialogFragment.kt
@@ -6,27 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import hous.release.android.R
-import hous.release.android.databinding.FragmentWarningDialogBinding
+import hous.release.android.databinding.DialogWarningBinding
+import hous.release.android.util.extension.initLayout
 
 class WarningDialogFragment : DialogFragment() {
-    private var _binding: FragmentWarningDialogBinding? = null
+    private var _binding: DialogWarningBinding? = null
     private val binding get() = _binding ?: error(getString(R.string.binding_error))
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentWarningDialogBinding.inflate(inflater, container, false)
+        _binding = DialogWarningBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initLayout()
         with(binding) {
             title = getString(R.string.edit_hous_name_warning_title)
             desc = getString(R.string.edit_hous_name_warning_desc)
             cancel = getString(R.string.edit_hous_name_warning_cancel)
             confirm = getString(R.string.edit_hous_name_warning_confirm)
+        }
+        initCancelClickListener()
+        initConfirmClickListener()
+    }
+
+    private fun initCancelClickListener() {
+        binding.tvWarningCancel.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun initConfirmClickListener() {
+        binding.tvWarningConfirm.setOnClickListener {
+            dismiss()
+            requireActivity().finish()
         }
     }
 }
