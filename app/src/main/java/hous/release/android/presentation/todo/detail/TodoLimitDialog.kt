f069431d2b235178f1ddde39535f893fda7f6c89
KWY0218 <wodrk1001@gmail.com>
2022-10-21 10:45:11 +0900

    #55 [feat] dialog fragment xml과 바인딩

---
+++ app/src/main/java/hous/release/android/presentation/todo/detail/TodoLimitDialog.kt
@@ -0,0 +1,42 @@
+package hous.release.android.presentation.todo.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import hous.release.android.R
+import hous.release.android.databinding.DialogTodoLimitBinding
+import hous.release.android.util.extension.initLayout
+import timber.log.Timber
+
+class TodoLimitDialog : DialogFragment() {
+    private var _binding: DialogTodoLimitBinding? = null
+    private val binding get() = _binding ?: error(getString(R.string.binding_error))
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogTodoLimitBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initLayout()
+        initCancelOnClick()
+    }
+
+    private fun initCancelOnClick() {
+        binding.tvTodoLimitConfirm.setOnClickListener {
+            dialog?.dismiss() ?: Timber.e(getString(R.string.null_point_exception))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
