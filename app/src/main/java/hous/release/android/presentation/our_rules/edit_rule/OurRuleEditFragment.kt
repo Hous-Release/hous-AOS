253b988fb17a2b534f84de1bdbd5e8c330d4eb42
murjune <sis9221302@gmail.com>
2022-10-23 22:01:55 +0900

    #60 [feat] 규칙 수정 뷰 서버통신 및 기능 구현

--- app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditFragment.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditFragment.kt
@@ -1,10 +1,72 @@
 package hous.release.android.presentation.our_rules.edit_rule
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentOurRuleEditBinding
+import hous.release.android.presentation.our_rules.adapter.OurRulesEditAdapter
+import hous.release.android.presentation.our_rules.type.SaveButtonState
+import hous.release.android.util.ItemTouchHelperCallback
 import hous.release.android.util.binding.BindingFragment
+import hous.release.android.util.extension.repeatOnStarted
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class OurRuleEditFragment :
-    BindingFragment<FragmentOurRuleEditBinding>(R.layout.fragment_our_rule_edit)
+    BindingFragment<FragmentOurRuleEditBinding>(R.layout.fragment_our_rule_edit) {
+    private val viewModel by viewModels<OurRuleEditViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+        initAdapter()
+        observeUiState()
+        initSaveButtonListener()
+    }
+
+    private fun initAdapter() {
+        val itemTouchHelper: ItemTouchHelper
+        OurRulesEditAdapter(viewModel::updateEditRuleList, viewModel::initRuleList)
+            .also { adapter ->
+                binding.rvEditOurRules.adapter = adapter
+            }.also { adapter ->
+                itemTouchHelper = ItemTouchHelper(ItemTouchHelperCallback(adapter)).apply {
+                    attachToRecyclerView(binding.rvEditOurRules)
+                }
+            }.also { adapter ->
+                adapter.startDrag(object : OurRulesEditAdapter.OnStartDragListener {
+                    override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
+                        itemTouchHelper.startDrag(viewHolder)
+                    }
+                })
+            }
+    }
+
+    private fun observeUiState() {
+        repeatOnStarted {
+            viewModel.uiState.collect { uiState ->
+                (binding.rvEditOurRules.adapter as OurRulesEditAdapter).submitList(uiState.ourEditRuleList)
+                if (viewModel.isChangeRuleList()) {
+                    viewModel.setSaveButtonState(SaveButtonState.ACTIVE)
+                } else {
+                    viewModel.setSaveButtonState(SaveButtonState.INACTIVE)
+                }
+            }
+        }
+    }
+
+    private fun initSaveButtonListener() {
+        binding.ivAddRuleSaveButton.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.putOurEditRules().join()
+                findNavController().popBackStack()
+            }
+        }
+    }
+}
--- app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditViewModel.kt
@@ -1,8 +1,101 @@
 package hous.release.android.presentation.our_rules.edit_rule
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hous.release.android.presentation.our_rules.type.SaveButtonState
+import hous.release.domain.entity.ApiResult
+import hous.release.domain.entity.response.OurRule
+import hous.release.domain.usecase.GetOurRulesUseCase
+import hous.release.domain.usecase.PutEditOurRulesUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class OurRuleEditViewModel @Inject constructor() : ViewModel()
+class OurRuleEditViewModel @Inject constructor(
+    private val getOurRulesUseCase: GetOurRulesUseCase,
+    private val putEditOurRulesUseCase: PutEditOurRulesUseCase
+) : ViewModel() {
+    private var _uiState = MutableStateFlow(OurRuleEditUIState())
+    val uiState: StateFlow<OurRuleEditUIState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getOurRulesUseCase().collect { apiResult ->
+                when (apiResult) {
+                    is ApiResult.Success ->
+                        _uiState.update { uiState ->
+                            val ourRuleList = initRuleList(apiResult.data)
+                            uiState.copy(isLoading = false, ourOriginalRuleList = ourRuleList, ourEditRuleList = ourRuleList)
+                        }
+                    is ApiResult.Empty -> {
+                        // TODO EMPTY 뷰 나오면 ERROR 로직 처리해주기
+                        _uiState.update { uiState ->
+                            uiState.copy(isEmpty = true, isLoading = false)
+                        }
+                    }
+                    is ApiResult.Error -> {
+                        _uiState.update { uiState ->
+                            uiState.copy(isError = true, isLoading = false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun initRuleList(ruleList: List<OurRule>) = ruleList
+        .mapIndexed { idx, ourRule ->
+            when (idx) {
+                in 0..1 -> ourRule.copy(ruleType = OurRule.RuleType.RepresentativeRule())
+                2 -> ourRule.copy(ruleType = OurRule.RuleType.RepresentativeRule(false))
+                else -> ourRule.copy(ruleType = OurRule.RuleType.GeneralRule())
+            }
+        }
+
+    fun updateEditRuleList(editList: List<OurRule>) {
+        _uiState.update { uiState ->
+            uiState.copy(ourEditRuleList = initRuleList(editList))
+        }
+        Timber.e(uiState.value.ourEditRuleList.toString())
+    }
+
+    fun isChangeRuleList(): Boolean {
+        val ourEditRuleList = uiState.value.ourEditRuleList
+        uiState.value.ourOriginalRuleList.forEachIndexed { index, ourRule ->
+            if (ourEditRuleList[index].id != ourRule.id) return true
+        }
+        return false
+    }
+
+    fun setSaveButtonState(saveButtonState: SaveButtonState) {
+        _uiState.update { uiState ->
+            uiState.copy(saveButtonState = saveButtonState)
+        }
+    }
+
+    fun putOurEditRules() =
+        viewModelScope.launch {
+            putEditOurRulesUseCase(editedRules = uiState.value.ourEditRuleList).collect { apiResult ->
+                when (apiResult) {
+                    is ApiResult.Success -> Timber.i(apiResult.data)
+                    is ApiResult.Error -> Timber.e(apiResult.throwable)
+                    is ApiResult.Empty -> Timber.e("IllegalArgument Exception")
+                }
+            }
+        }
+
+    data class OurRuleEditUIState(
+        val ourOriginalRuleList: List<OurRule> = emptyList(),
+        val ourEditRuleList: List<OurRule> = emptyList(),
+        val isError: Boolean = false,
+        val isEmpty: Boolean = false,
+        val isLoading: Boolean = true,
+        val saveButtonState: SaveButtonState = SaveButtonState.INACTIVE
+    )
+}
