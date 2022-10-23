de5d38976b9e8b871319f97c2d9f352b6696fd5b
murjune <sis9221302@gmail.com>
2022-10-23 21:58:33 +0900

    #60 [feat] OurRule 함수 추가

--- domain/src/main/java/hous/release/domain/entity/response/OurRule.kt
+++ domain/src/main/java/hous/release/domain/entity/response/OurRule.kt
@@ -3,10 +3,20 @@ package hous.release.domain.entity.response
 data class OurRule(
     val id: Int = -1,
     val name: String = "",
-    val ruleType: RuleType = RuleType.GeneralRule
+    val ruleType: RuleType = RuleType.GeneralRule()
 ) {
     sealed class RuleType {
         data class RepresentativeRule(val hasDivider: Boolean = true) : RuleType()
-        object GeneralRule : RuleType()
+        data class GeneralRule(val hasDivider: Boolean = true) : RuleType()
+    }
+
+    fun toNoDividerRule(ruleType: RuleType) = when (ruleType) {
+        is RuleType.RepresentativeRule -> copy(ruleType = RuleType.RepresentativeRule(false))
+        is RuleType.GeneralRule -> copy(ruleType = RuleType.RepresentativeRule(false))
+    }
+
+    fun toDividerRule(ruleType: RuleType) = when (ruleType) {
+        is RuleType.RepresentativeRule -> copy(ruleType = RuleType.RepresentativeRule(true))
+        is RuleType.GeneralRule -> copy(ruleType = RuleType.RepresentativeRule(true))
     }
 }
