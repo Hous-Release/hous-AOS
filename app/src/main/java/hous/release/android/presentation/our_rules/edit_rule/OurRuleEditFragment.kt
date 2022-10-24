e1b23a151f51245784e86b1b66c69179080efa21
murjune <sis9221302@gmail.com>
2022-10-24 22:40:09 +0900

    #60 [feat] backButton 클릭 시 outDialog 로직 구현

--- app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditFragment.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/edit_rule/OurRuleEditFragment.kt
@@ -2,6 +2,7 @@ package hous.release.android.presentation.our_rules.edit_rule
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -14,7 +15,11 @@ import hous.release.android.presentation.our_rules.adapter.OurRulesEditAdapter
 import hous.release.android.presentation.our_rules.type.SaveButtonState
 import hous.release.android.util.ItemTouchHelperCallback
 import hous.release.android.util.binding.BindingFragment
+import hous.release.android.util.dialog.ConfirmClickListener
+import hous.release.android.util.dialog.WarningDialogFragment
+import hous.release.android.util.dialog.WarningType
 import hous.release.android.util.extension.repeatOnStarted
+import hous.release.android.util.extension.withArgs
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -25,9 +30,47 @@ class OurRuleEditFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
+        initBackBtnClickListener()
         initAdapter()
         collectUiState()
         initSaveButtonListener()
+    }
+
+    private fun initBackBtnClickListener() {
+        binding.ivEditRuleBackButton.setOnClickListener {
+            if (viewModel.uiState.value.saveButtonState == SaveButtonState.ACTIVE) {
+                showOutDialog()
+                return@setOnClickListener
+            }
+            findNavController().popBackStack()
+        }
+        requireActivity().onBackPressedDispatcher.apply {
+            addCallback {
+                if (!this@OurRuleEditFragment.isAdded) {
+                    this.remove()
+                    this@apply.onBackPressed()
+                    return@addCallback
+                }
+                if (viewModel.uiState.value.saveButtonState == SaveButtonState.ACTIVE) {
+                    showOutDialog()
+                    return@addCallback
+                }
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun showOutDialog() {
+        WarningDialogFragment().withArgs {
+            putSerializable(
+                WarningDialogFragment.WARNING_TYPE,
+                WarningType.WARNING_EDIT_RULE
+            )
+            putParcelable(
+                WarningDialogFragment.CONFIRM_ACTION,
+                ConfirmClickListener(confirmAction = { findNavController().popBackStack() })
+            )
+        }.show(childFragmentManager, WarningDialogFragment.DIALOG_WARNING)
     }
 
     private fun initAdapter() {
