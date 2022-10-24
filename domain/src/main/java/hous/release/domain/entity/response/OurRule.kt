be2691f3f010b559d77e1a129661b52694760c42
murjune <sis9221302@gmail.com>
2022-10-24 20:50:19 +0900

    #60 [fix] OurRule 수정

--- domain/src/main/java/hous/release/domain/entity/response/OurRule.kt
+++ domain/src/main/java/hous/release/domain/entity/response/OurRule.kt
@@ -1,22 +1,9 @@
 package hous.release.domain.entity.response
 
+import hous.release.domain.entity.RuleType
+
 data class OurRule(
     val id: Int = -1,
     val name: String = "",
-    val ruleType: RuleType = RuleType.GeneralRule()
-) {
-    sealed class RuleType {
-        data class RepresentativeRule(val hasDivider: Boolean = true) : RuleType()
-        data class GeneralRule(val hasDivider: Boolean = true) : RuleType()
-    }
-
-    fun toNoDividerRule(ruleType: RuleType) = when (ruleType) {
-        is RuleType.RepresentativeRule -> copy(ruleType = RuleType.RepresentativeRule(false))
-        is RuleType.GeneralRule -> copy(ruleType = RuleType.RepresentativeRule(false))
-    }
-
-    fun toDividerRule(ruleType: RuleType) = when (ruleType) {
-        is RuleType.RepresentativeRule -> copy(ruleType = RuleType.RepresentativeRule(true))
-        is RuleType.GeneralRule -> copy(ruleType = RuleType.RepresentativeRule(true))
-    }
-}
+    val ruleType: RuleType = RuleType.GENERAL
+)
