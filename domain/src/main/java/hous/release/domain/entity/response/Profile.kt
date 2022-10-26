559b48dbc02eb5b954ed86ce2fab96fdde53b77b
이빵주 <yungju9982@naver.com>
2022-10-26 10:18:25 +0900

    #83 [add] Profile response 생성

---
+++ domain/src/main/java/hous/release/domain/entity/response/Profile.kt
@@ -0,0 +1,24 @@
+package hous.release.domain.entity.response
+
+data class Profile(
+    val age: String = "",
+    val birthday: String? = "",
+    val birthdayPublic: Boolean = false,
+    val introduction: String? = "",
+    val job: String? = "",
+    val mbti: String? = "",
+    val nickname: String = "",
+    val personalityColor: String = "",
+    val representBadge: String? = "",
+    val representBadgeImage: String = "",
+    val testScore: TestScore = TestScore()
+
+)
+
+data class TestScore(
+    val clean: Int = 0,
+    val introversion: Int = 0,
+    val light: Int = 0,
+    val noise: Int = 0,
+    val smell: Int = 0
+)
