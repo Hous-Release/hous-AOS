f8f08b429d20a0571cf7e86fb033f881a1e5d59f
murjune <sis9221302@gmail.com>
2022-10-27 02:26:57 +0900

    #60 [fix] api 응답값 BaseResponse<Unit> -> NoDataResponse 로 변경

--- data/src/main/java/hous/release/data/datasource/OurRulesDataSource.kt
+++ data/src/main/java/hous/release/data/datasource/OurRulesDataSource.kt
@@ -3,6 +3,7 @@ package hous.release.data.datasource
 import hous.release.data.entity.request.AddRulesRequest
 import hous.release.data.entity.request.EditRulesRequest
 import hous.release.data.entity.response.BaseResponse
+import hous.release.data.entity.response.NoDataResponse
 import hous.release.data.entity.response.OurRulesResponse
 import hous.release.data.service.OurRulesService
 import javax.inject.Inject
@@ -12,9 +13,9 @@ class OurRulesDataSource @Inject constructor(private val ourRulesService: OurRul
     suspend fun getOurRulesContent(): BaseResponse<List<OurRulesResponse>> =
         ourRulesService.getOurRuleContent()
 
-    suspend fun postAddedRuleContent(addedRules: List<String>): BaseResponse<Unit> =
+    suspend fun postAddedRuleContent(addedRules: List<String>): NoDataResponse =
         ourRulesService.postAddedRuleContent(AddRulesRequest(addedRules = addedRules))
 
-    suspend fun putEditedRuleContent(editedRules: List<Int>): BaseResponse<Unit> =
+    suspend fun putEditedRuleContent(editedRules: List<Int>): NoDataResponse =
         ourRulesService.putEditedRuleContent(EditRulesRequest(rulesIdList = editedRules))
 }
--- data/src/main/java/hous/release/data/service/OurRulesService.kt
+++ data/src/main/java/hous/release/data/service/OurRulesService.kt
@@ -3,6 +3,7 @@ package hous.release.data.service
 import hous.release.data.entity.request.AddRulesRequest
 import hous.release.data.entity.request.EditRulesRequest
 import hous.release.data.entity.response.BaseResponse
+import hous.release.data.entity.response.NoDataResponse
 import hous.release.data.entity.response.OurRulesResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,8 +15,8 @@ interface OurRulesService {
     suspend fun getOurRuleContent(): BaseResponse<List<OurRulesResponse>>
 
     @POST("/v1/rules")
-    suspend fun postAddedRuleContent(@Body body: AddRulesRequest): BaseResponse<Unit>
+    suspend fun postAddedRuleContent(@Body body: AddRulesRequest): NoDataResponse
 
     @PUT("/v1/rules")
-    suspend fun putEditedRuleContent(@Body body: EditRulesRequest): BaseResponse<Unit>
+    suspend fun putEditedRuleContent(@Body body: EditRulesRequest): NoDataResponse
 }
