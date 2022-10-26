8ccbed94b1202e1055a1cf0db0c81a0b8b9cd2d7
이빵주 <yungju9982@naver.com>
2022-10-26 10:26:49 +0900

    #74 [fix] DatePicker initLayout 호출 위치 변경

--- app/src/main/java/hous/release/android/presentation/login/DatePickerDialog.kt
+++ app/src/main/java/hous/release/android/presentation/login/DatePickerDialog.kt
@@ -25,8 +25,8 @@ class DatePickerDialog(private val doAfterConfirm: (date: String) -> Unit) :
         } ?: throw IllegalStateException()
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initLayout()
     }
 
