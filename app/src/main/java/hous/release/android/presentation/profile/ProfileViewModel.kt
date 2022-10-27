d37260b795a1e867f8a9671e0ba48980368c5d79
이빵주 <yungju9982@naver.com>
2022-10-28 04:11:25 +0900

    #88 [fix] ProfileViewModel에서 서버통신

--- app/src/main/java/hous/release/android/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/profile/ProfileViewModel.kt
@@ -5,42 +5,36 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hous.release.domain.entity.response.Profile
+import hous.release.domain.repository.ProfileRepository
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
-    private val _age = MutableLiveData<String>()
-    val age: LiveData<String> = _age
-
-    private val _birthday = MutableLiveData<String>()
-    val birthday: LiveData<String> = _birthday
-
-    private val _introduction = MutableLiveData<String>()
-    val introduction: LiveData<String> = _introduction
-
-    private val _job = MutableLiveData<String>()
-    val job: LiveData<String> = _job
-
-    private val _mbti = MutableLiveData<String>()
-    val mbti: LiveData<String> = _mbti
-
-    private val _nickname = MutableLiveData<String>()
-    val nickname: LiveData<String> = _nickname
-
-    private val _personalityColor = MutableLiveData<String>()
-    val personalityColor: LiveData<String> = _personalityColor
+class ProfileViewModel @Inject constructor(
+    private val profileRepository: ProfileRepository
+) : ViewModel() {
+    private val _profileData = MutableLiveData<Profile>()
+    val profileData get() = _profileData
 
     private val _isTest = MutableLiveData<Boolean>()
     val isTest: LiveData<Boolean> = _isTest
 
     init {
         viewModelScope.launch {
-            _age.value = "23세"
-            _job.value = "개백수"
-            _nickname.value = "이빵주"
-            _personalityColor.value = "GRAY"
-            _isTest.value = _personalityColor.value != "GRAY"
+            profileRepository.getUser()
+                .onSuccess {
+                    _profileData.value = it
+                }.onFailure {
+                    Timber.e("${it.message}")
+                }
+        }
+    }
+
+    fun checkTest() {
+        viewModelScope.launch {
+            _isTest.value = _profileData.value!!.testScore != Profile.TestScore(0, 0, 0, 0, 0)
         }
     }
 }
