3360f0bdc7fb057c24f1ac0ea7a1f7a0dedc9c05
murjune <sis9221302@gmail.com>
2022-10-28 17:47:45 +0900

    #78 [fix] dispatcher 모듈 삭제

--- app/src/main/java/hous/release/android/di/RepositoryModule.kt
+++ app/src/main/java/hous/release/android/di/RepositoryModule.kt
@@ -20,7 +20,6 @@ import hous.release.domain.repository.EnterRoomRepository
 import hous.release.domain.repository.HousRepository
 import hous.release.domain.repository.OurRulesRepository
 import hous.release.domain.repository.TodoRepository
-import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Singleton
 
 @Module
@@ -52,10 +51,9 @@ class RepositoryModule {
     @Provides
     @Singleton
     fun providesOurRulesRepository(
-        ourRulesDataSource: OurRulesDataSource,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
+        ourRulesDataSource: OurRulesDataSource
     ): OurRulesRepository =
-        OurRulesRepositoryImpl(ourRulesDataSource, coroutineDispatcher)
+        OurRulesRepositoryImpl(ourRulesDataSource)
 
     @Provides
     @Singleton
