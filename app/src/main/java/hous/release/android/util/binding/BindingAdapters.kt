5981b6c93f6b98f225d9bcfdd623134f0b9c5a63
이빵주 <yungju9982@naver.com>
2022-10-29 07:18:01 +0900

    #93 [add] PersonalityResult 컬러값 바인딩어댑터 생성

--- app/src/main/java/hous/release/android/util/binding/BindingAdapters.kt
+++ app/src/main/java/hous/release/android/util/binding/BindingAdapters.kt
@@ -1,9 +1,12 @@
 package hous.release.android.util.binding
 
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import hous.release.android.R
+import hous.release.domain.entity.HomyType
 import hous.release.domain.entity.NotificationType
 
 object BindingAdapters {
@@ -27,5 +30,38 @@ object BindingAdapters {
                 .load(imgUrl)
                 .into(this)
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter("personalityResultColor")
+    fun TextView.setPersonalityResultColor(color: String) {
+        setTextColor(
+            ContextCompat.getColor(
+                context,
+                when (HomyType.valueOf(color)) {
+                    HomyType.RED -> R.color.hous_red
+                    HomyType.YELLOW -> R.color.hous_yellow
+                    HomyType.GREEN -> R.color.hous_green
+                    HomyType.BLUE -> R.color.hous_blue
+                    HomyType.PURPLE -> R.color.hous_purple
+                    else -> -1
+                }
+            )
+        )
+    }
+
+    @JvmStatic
+    @BindingAdapter("personalityResultIcon")
+    fun ImageView.setPersonalityResultIcon(color: String) {
+        setImageResource(
+            when (HomyType.valueOf(color)) {
+                HomyType.RED -> R.drawable.ic_personality_result_check_red
+                HomyType.YELLOW -> R.drawable.ic_personality_result_check_yellow
+                HomyType.GREEN -> R.drawable.ic_personality_result_check_green
+                HomyType.BLUE -> R.drawable.ic_personality_result_check_blue
+                HomyType.PURPLE -> R.drawable.ic_personality_result_check_purple
+                else -> -1
+            }
+        )
     }
 }
