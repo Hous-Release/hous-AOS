6c79adf2b6b797add12e533fed0cf8b08d3d0014
이빵주 <yungju9982@naver.com>
2022-10-29 09:50:24 +0900

    #93 [fix] 바인딩어댑터 파라미터 nullable 처리

--- app/src/main/java/hous/release/android/util/binding/BindingAdapters.kt
+++ app/src/main/java/hous/release/android/util/binding/BindingAdapters.kt
@@ -34,34 +34,38 @@ object BindingAdapters {
 
     @JvmStatic
     @BindingAdapter("personalityResultColor")
-    fun TextView.setPersonalityResultColor(color: String) {
-        setTextColor(
-            ContextCompat.getColor(
-                context,
-                when (HomyType.valueOf(color)) {
-                    HomyType.RED -> R.color.hous_red
-                    HomyType.YELLOW -> R.color.hous_yellow
-                    HomyType.GREEN -> R.color.hous_green
-                    HomyType.BLUE -> R.color.hous_blue
-                    HomyType.PURPLE -> R.color.hous_purple
-                    HomyType.GRAY -> -1
-                }
+    fun TextView.setPersonalityResultColor(color: String?) {
+        color?.let {
+            setTextColor(
+                ContextCompat.getColor(
+                    context,
+                    when (HomyType.valueOf(color)) {
+                        HomyType.RED -> R.color.hous_red
+                        HomyType.YELLOW -> R.color.hous_yellow
+                        HomyType.GREEN -> R.color.hous_green
+                        HomyType.BLUE -> R.color.hous_blue
+                        HomyType.PURPLE -> R.color.hous_purple
+                        HomyType.GRAY -> -1
+                    }
+                )
             )
-        )
+        }
     }
 
     @JvmStatic
     @BindingAdapter("personalityResultIcon")
-    fun ImageView.setPersonalityResultIcon(color: String) {
-        setImageResource(
-            when (HomyType.valueOf(color)) {
-                HomyType.RED -> R.drawable.ic_personality_result_check_red
-                HomyType.YELLOW -> R.drawable.ic_personality_result_check_yellow
-                HomyType.GREEN -> R.drawable.ic_personality_result_check_green
-                HomyType.BLUE -> R.drawable.ic_personality_result_check_blue
-                HomyType.PURPLE -> R.drawable.ic_personality_result_check_purple
-                HomyType.GRAY -> -1
-            }
-        )
+    fun ImageView.setPersonalityResultIcon(color: String?) {
+        color?.let {
+            setImageResource(
+                when (HomyType.valueOf(color)) {
+                    HomyType.RED -> R.drawable.ic_personality_result_check_red
+                    HomyType.YELLOW -> R.drawable.ic_personality_result_check_yellow
+                    HomyType.GREEN -> R.drawable.ic_personality_result_check_green
+                    HomyType.BLUE -> R.drawable.ic_personality_result_check_blue
+                    HomyType.PURPLE -> R.drawable.ic_personality_result_check_purple
+                    HomyType.GRAY -> -1
+                }
+            )
+        }
     }
 }
