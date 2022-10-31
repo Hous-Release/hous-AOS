fa6d9c8da7ee61f80816c31074391ab2d91bd856
murjune <sis9221302@gmail.com>
2022-10-31 20:13:52 +0900

    #96 [feat] collectAsState.. 확장함수 추가

--- app/src/main/java/hous/release/android/util/extension/LifecycleExtension.kt
+++ app/src/main/java/hous/release/android/util/extension/LifecycleExtension.kt
@@ -1,11 +1,18 @@
 package hous.release.android.util.extension
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 /**
@@ -30,4 +37,25 @@ inline fun LifecycleOwner.repeatOnStarted(crossinline block: suspend () -> Unit)
             }
         }
     }
+}
+
+/**
+ * composable이 보이지 않을 때도 계속 값을 수집한다는 문제가 있다.
+ * 우리는 Flow에 생명주기를 입혀 Lifecycledl STARTED일 때만 수집 가능하게 만들어주는 확장함수
+ *
+ * @param T
+ * @param initial
+ * @param minActiveState
+ * @return State<T>
+ */
+@Composable
+fun <T> Flow<T>.collectAsStateWithLifecycleRemember(
+    initial: T,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED
+): State<T> {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val flowLifecycleAware = remember(this, lifecycleOwner) {
+        flowWithLifecycle(lifecycleOwner.lifecycle, minActiveState)
+    }
+    return flowLifecycleAware.collectAsState(initial)
 }
