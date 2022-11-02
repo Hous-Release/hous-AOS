1a730074d21f2b10d5371d3d67acb7ba29f93657
Yunjoo Son <thsduswn4141@naver.com>
2022-11-02 21:58:31 +0900

    #97 [ui] 알림설정화면 레이아웃

---
+++ app/src/main/java/hous/release/android/presentation/settings/NotificationSettingsActivity.kt
@@ -0,0 +1,13 @@
+package hous.release.android.presentation.settings
+
+import android.os.Bundle
+import hous.release.android.R
+import hous.release.android.databinding.ActivityNotificationSettingsBinding
+import hous.release.android.util.binding.BindingActivity
+
+class NotificationSettingsActivity :
+    BindingActivity<ActivityNotificationSettingsBinding>(R.layout.activity_notification_settings) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
