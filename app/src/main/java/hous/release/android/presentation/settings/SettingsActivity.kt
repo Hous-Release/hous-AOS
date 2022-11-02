b4322db59407601fb962e4509b6902dec25103ac
Yunjoo Son <thsduswn4141@naver.com>
2022-11-02 23:48:16 +0900

    #98 [feat] 방참여/만들기>설정 화면 이동 구현

--- app/src/main/java/hous/release/android/presentation/settings/SettingsActivity.kt
+++ app/src/main/java/hous/release/android/presentation/settings/SettingsActivity.kt
@@ -9,8 +9,13 @@ import hous.release.android.util.binding.BindingActivity
 class SettingsActivity : BindingActivity<ActivitySettingsBinding>(R.layout.activity_settings) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initSettingsMode()
         initBackBtnClickListener()
         initNotiSettingClickListener()
+    }
+
+    private fun initSettingsMode() {
+        binding.hasRoom = intent.getBooleanExtra(HAS_ROOM, true)
     }
 
     private fun initBackBtnClickListener() {
@@ -21,5 +26,9 @@ class SettingsActivity : BindingActivity<ActivitySettingsBinding>(R.layout.activ
         binding.btnSettingsNotification.setOnClickListener {
             startActivity(Intent(this, NotificationSettingsActivity::class.java))
         }
+    }
+
+    companion object {
+        const val HAS_ROOM = "HAS_ROOM"
     }
 }
