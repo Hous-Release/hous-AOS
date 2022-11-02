3e1516ba5707ef56ebf61574f508266a8ea8b989
Yunjoo Son <thsduswn4141@naver.com>
2022-11-02 16:51:38 +0900

    #97 [ui] 설정화면 레이아웃

---
+++ app/src/main/java/hous/release/android/presentation/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package hous.release.android.presentation.settings
+
+import android.os.Bundle
+import hous.release.android.R
+import hous.release.android.databinding.ActivitySettingsBinding
+import hous.release.android.util.binding.BindingActivity
+
+class SettingsActivity : BindingActivity<ActivitySettingsBinding>(R.layout.activity_settings) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
