273a3cd9e4628105ad7ed9b95c2a90747e266bc3
murjune <sis9221302@gmail.com>
2022-11-04 05:32:08 +0900

    #107 [feat] todo post, get ViewModel과 연동

--- app/src/main/java/hous/release/android/presentation/todo/add/AddToDoVIewModel.kt
+++ app/src/main/java/hous/release/android/presentation/todo/add/AddToDoVIewModel.kt
@@ -4,23 +4,48 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hous.release.android.presentation.todo.viewmodel.UpdateToDoUiState
 import hous.release.android.presentation.todo.viewmodel.UpdateToDoViewModel
+import hous.release.domain.entity.ApiResult
 import hous.release.domain.entity.HomyType
 import hous.release.domain.entity.response.ToDoUser
-import kotlinx.coroutines.delay
+import hous.release.domain.usecase.GetToDoUsersUseCase
+import hous.release.domain.usecase.PostAddToDoUseCase
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class AddToDoVIewModel @Inject constructor() : UpdateToDoViewModel() {
+class AddToDoVIewModel @Inject constructor(
+    private val getToDoUsersUseCase: GetToDoUsersUseCase,
+    private val postAddToDoUseCase: PostAddToDoUseCase
+) : UpdateToDoViewModel() {
 
     init {
-        // TODO 추후 서버 통신
-        _uiState.value = dummyUiState
+        viewModelScope.launch {
+            getToDoUsersUseCase().collect { apiResult ->
+                when (apiResult) {
+                    is ApiResult.Success -> _uiState.update { uiState ->
+                        uiState.copy(todoUsers = apiResult.data)
+                    }
+                    is ApiResult.Error -> Timber.e(apiResult.msg)
+                    is ApiResult.Empty -> Timber.i("empty View")
+                }
+            }
+        }
     }
 
     fun putTodo() = viewModelScope.launch {
-        // TODO 추후 서버 통신
-        delay(500L)
+        postAddToDoUseCase(
+            isPushNotification = uiState.value.isPushNotification,
+            todoUsers = uiState.value.selectedUsers,
+            toDoName = todoName.value
+        ).collect { apiResult ->
+            when (apiResult) {
+                is ApiResult.Success -> Timber.i(apiResult.data)
+                is ApiResult.Error -> Timber.e(apiResult.msg)
+                is ApiResult.Empty -> Timber.e(IllegalArgumentException())
+            }
+        }
     }
 
     companion object {
