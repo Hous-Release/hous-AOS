ce499369365924a8077ffc00a3a7a044c7263724
KWY0218 <wodrk1001@gmail.com>
2022-11-08 14:42:28 +0900

    #115 [ui] 성향 테스트 뷰 구현

---
+++ app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestFragment.kt
@@ -0,0 +1,8 @@
+package hous.release.android.presentation.personality.test
+
+import hous.release.android.R
+import hous.release.android.databinding.FragmentPersonalityTestBinding
+import hous.release.android.util.binding.BindingFragment
+
+class PersonalityTestFragment :
+    BindingFragment<FragmentPersonalityTestBinding>(R.layout.fragment_personality_test)
