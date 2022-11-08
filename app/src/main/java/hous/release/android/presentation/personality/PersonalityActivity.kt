c47446c9b5ab8fa44ebbd2a87d529fedca7d1435
KWY0218 <wodrk1001@gmail.com>
2022-11-08 10:15:07 +0900

    #115 [ui] 성향 테스트 시작 뷰 구현

---
+++ app/src/main/java/hous/release/android/presentation/personality/PersonalityActivity.kt
@@ -0,0 +1,12 @@
+package hous.release.android.presentation.personality
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hous.release.android.R
+
+class PersonalityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personality)
+    }
+}
---
+++ app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestStartFragment.kt
@@ -0,0 +1,8 @@
+package hous.release.android.presentation.personality.test
+
+import hous.release.android.R
+import hous.release.android.databinding.FragmentPersonalityTestStartBinding
+import hous.release.android.util.binding.BindingFragment
+
+class PersonalityTestStartFragment :
+    BindingFragment<FragmentPersonalityTestStartBinding>(R.layout.fragment_personality_test_start)
