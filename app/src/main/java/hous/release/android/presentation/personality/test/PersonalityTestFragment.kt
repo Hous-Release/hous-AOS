4c0f31bccbef27050862bc93d19b505db3da9699
KWY0218 <wodrk1001@gmail.com>
2022-11-08 15:49:22 +0900

    #115 [feat] 테스트 결과 화면으로 이동 로직 구현

--- app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestFragment.kt
+++ app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestFragment.kt
@@ -1,14 +1,28 @@
 package hous.release.android.presentation.personality.test
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import hous.release.android.R
 import hous.release.android.databinding.FragmentPersonalityTestBinding
+import hous.release.android.presentation.personality.result.PersonalityResultActivity
+import hous.release.android.presentation.personality.result.PersonalityResultActivity.Companion.LOCATION
+import hous.release.android.presentation.personality.result.PersonalityResultActivity.Companion.RESULT
 import hous.release.android.util.binding.BindingFragment
 
 class PersonalityTestFragment :
     BindingFragment<FragmentPersonalityTestBinding>(R.layout.fragment_personality_test) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        goPersonalityTestResult()
+    }
+
+    private fun goPersonalityTestResult() {
+        Intent(requireActivity(), PersonalityResultActivity::class.java).apply {
+            putExtra(LOCATION, RESULT)
+        }.also { intent ->
+            requireActivity().finish()
+            startActivity(intent)
+        }
     }
 }
