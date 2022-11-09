1780242cdbe708202df696a0d2c3f6fe99692309
KWY0218 <wodrk1001@gmail.com>
2022-11-09 14:17:04 +0900

    #117 [feat] 성향 테스트 uiState 와 연결

--- app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestViewModel.kt
@@ -1,20 +1,43 @@
 package hous.release.android.presentation.personality.test
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hous.release.domain.entity.PersonalityTest
 import hous.release.domain.entity.QuestionType
 import hous.release.domain.usecase.GetPersonalityTestsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class PersonalityTestViewModel @Inject constructor(
     private val getPersonalityTestsUseCase: GetPersonalityTestsUseCase,
     private val putPersonalityTestsUseCase: GetPersonalityTestsUseCase
-) : ViewModel()
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(PersonalityTestUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getPersonalityTestsUseCase()
+                .onSuccess { personalityTests ->
+                    _uiState.update { uiState ->
+                        uiState.copy(personalityTests = personalityTests)
+                    }
+                }
+                .onFailure {
+                    Timber.e(it.message)
+                }
+        }
+    }
+}
 
 data class PersonalityTestUiState(
-    val personalityTests: List<PersonalityTest>,
+    val personalityTests: List<PersonalityTest> = emptyList(),
     val testScore: HashMap<QuestionType, Int> = hashMapOf(
         QuestionType.LIGHT to 0,
         QuestionType.NOISE to 0,
