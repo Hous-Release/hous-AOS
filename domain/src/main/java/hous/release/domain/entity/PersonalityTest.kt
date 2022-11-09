5d69a114aad52624fb95e0300033345da033ec22
KWY0218 <wodrk1001@gmail.com>
2022-11-09 14:05:13 +0900

    #117 [feat] test 상태 enum class 추가

--- domain/src/main/java/hous/release/domain/entity/PersonalityTest.kt
+++ domain/src/main/java/hous/release/domain/entity/PersonalityTest.kt
@@ -5,5 +5,10 @@ data class PersonalityTest(
     val imageUrl: String,
     val index: Int,
     val question: List<String>,
-    val questionType: String
+    val questionType: String,
+    val testState: TestState = TestState.UNCHECKED
 )
+
+enum class TestState {
+    UNCHECKED, ONE, TWO, THREE
+}
