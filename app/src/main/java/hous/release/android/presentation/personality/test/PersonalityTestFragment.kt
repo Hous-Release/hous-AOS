5bf8ee3520083ef61e8f67ef7d12308ca3d4cfda
KWY0218 <wodrk1001@gmail.com>
2022-11-10 13:26:20 +0900

    #117 [feat] 백버튼 클릭 시 다이얼로그 호출 구현

--- app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestFragment.kt
+++ app/src/main/java/hous/release/android/presentation/personality/test/PersonalityTestFragment.kt
@@ -3,6 +3,8 @@ package hous.release.android.presentation.personality.test
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
@@ -14,6 +16,9 @@ import hous.release.android.presentation.personality.result.PersonalityResultAct
 import hous.release.android.presentation.personality.result.PersonalityResultActivity.Companion.RESULT
 import hous.release.android.presentation.personality.result.PersonalityResultActivity.Companion.RESULT_COLOR
 import hous.release.android.util.binding.BindingFragment
+import hous.release.android.util.dialog.ConfirmClickListener
+import hous.release.android.util.dialog.WarningDialogFragment
+import hous.release.android.util.dialog.WarningType
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -22,12 +27,14 @@ class PersonalityTestFragment :
     BindingFragment<FragmentPersonalityTestBinding>(R.layout.fragment_personality_test) {
     private val personalityTestViewModel: PersonalityTestViewModel by viewModels()
     private lateinit var personalityTestAdapter: PersonalityTestAdapter
+    private lateinit var onBackPressedCallback: OnBackPressedCallback
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
         collectPersonalityTests()
         collectMoveEvent()
+        initBackBtnOnClick()
     }
 
     private fun initAdapter() {
@@ -79,5 +86,30 @@ class PersonalityTestFragment :
             requireActivity().finish()
             startActivity(intent)
         }
+    }
+
+    private fun initBackBtnOnClick() {
+        requireActivity().onBackPressedDispatcher.apply {
+            addCallback {
+                showExitDialog()
+            }.also { callback ->
+                onBackPressedCallback = callback
+            }
+        }
+    }
+
+    private fun showExitDialog() {
+        WarningDialogFragment().apply {
+            arguments = Bundle().apply {
+                putSerializable(
+                    WarningDialogFragment.WARNING_TYPE,
+                    WarningType.WARNING_STOP_TEST
+                )
+                putParcelable(
+                    WarningDialogFragment.CONFIRM_ACTION,
+                    ConfirmClickListener(confirmAction = { requireActivity().finish() })
+                )
+            }
+        }.show(childFragmentManager, WarningDialogFragment.DIALOG_WARNING)
     }
 }
