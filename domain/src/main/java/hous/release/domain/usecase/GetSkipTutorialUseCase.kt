7208c3c5d1de57e36b4ca3ccc57bfec2933b9bd9
KWY0218 <wodrk1001@gmail.com>
2022-11-16 19:41:03 +0900

    #101 [delete] 불필요한 코드 제거

--- domain/src/main/java/hous/release/domain/usecase/GetSkipTutorialUseCase.kt
+++
@@ -1,8 +0,0 @@
-package hous.release.domain.usecase
-
-import hous.release.domain.repository.AuthRepository
-import javax.inject.Inject
-
-class GetSkipTutorialUseCase @Inject constructor(private val authRepository: AuthRepository) {
-    operator fun invoke() = authRepository.getIsSkipTutorial()
-}
