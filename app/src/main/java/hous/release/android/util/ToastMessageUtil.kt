ac4c148c33657f8ffad5f15396e6d4590d688b8b
Yunjoo Son <thsduswn4141@naver.com>
2022-12-04 18:18:22 +0900

    #154 [fix] 우리집 별명 수정 시 - 수정내역 있을 시에만 다이얼로그 띄우도록 수정

--- app/src/main/java/hous/release/android/util/ToastMessageUtil.kt
+++ app/src/main/java/hous/release/android/util/ToastMessageUtil.kt
@@ -2,15 +2,11 @@ package hous.release.android.util
 
 import android.content.Context
 import android.widget.Toast
-import timber.log.Timber
 
 object ToastMessageUtil {
     private var toast: Toast? = null
 
     fun showToast(context: Context, msg: String) {
-        if (toast == null) {
-            Timber.d("toast null")
-        }
         toast?.cancel()
         toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT)
         requireNotNull(toast).show()
