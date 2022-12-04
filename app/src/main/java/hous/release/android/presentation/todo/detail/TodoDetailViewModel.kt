8b856da521d21cc7688a39959eddba43b0940c4e
KWY <wodrk1001@gmail.com>
2022-12-04 17:42:04 +0900

    #152 [fix] todo 개수 총합하는 로직 수정

--- app/src/main/java/hous/release/android/presentation/todo/detail/TodoDetailViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/todo/detail/TodoDetailViewModel.kt
@@ -8,13 +8,13 @@ import hous.release.domain.entity.response.TodoMain
 import hous.release.domain.usecase.DeleteTodoUseCase
 import hous.release.domain.usecase.GetDailyTodosUseCase
 import hous.release.domain.usecase.GetMemberTodosUseCase
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltViewModel
 class TodoDetailViewModel @Inject constructor(
@@ -37,9 +37,9 @@ class TodoDetailViewModel @Inject constructor(
         viewModelScope.launch {
             getMemberTodosUseCase()
                 .onSuccess { result ->
-                    _uiState.value = _uiState.value.copy(memberToDos = result)
-                    val count = uiState.value.memberToDos.sumOf { todo -> todo.totalTodoCnt }
-                    _uiState.value = _uiState.value.copy(totalTodoCount = count)
+                    val count = result.sumOf { todo -> todo.totalTodoCnt }
+                    _uiState.value =
+                        _uiState.value.copy(memberToDos = result, totalTodoCount = count)
                 }
                 .onFailure {
                     Timber.e("error: ${it.message}")
