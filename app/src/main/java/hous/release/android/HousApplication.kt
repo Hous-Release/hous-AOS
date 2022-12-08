80fcb8a576624f7c8721956fc08c9028fd2a24b2
KWY <wodrk1001@gmail.com>
2022-12-09 01:05:31 +0900

    #162 [QA] hash key log

--- app/src/main/java/hous/release/android/HousApplication.kt
+++ app/src/main/java/hous/release/android/HousApplication.kt
@@ -1,10 +1,13 @@
 package hous.release.android
 
 import android.app.Application
+import android.util.Log
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.HiltAndroidApp
 import hous.release.android.BuildConfig.KAKAO_NATIVE_APP_KEY
 import hous.release.android.util.HousDeBugTree
@@ -14,6 +17,9 @@ import timber.log.Timber
 class HousApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        var keyHash = Utility.getKeyHash(this)
+        Timber.d("keyHash $keyHash")
+        Log.d("KEYHASH", "KEYHASH: $keyHash")
         KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
         if (BuildConfig.DEBUG) Timber.plant(HousDeBugTree())
         else Firebase.analytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, null)
