8be770bb41b6bd43b64febdac48803834b2693f7
Yunjoo Son <thsduswn4141@naver.com>
2022-12-10 18:58:42 +0900

    #168 [fix] delay 삭제 후 로티 애니메이션 리스너 추가

--- app/src/main/java/hous/release/android/presentation/splash/IntroActivity.kt
+++ app/src/main/java/hous/release/android/presentation/splash/IntroActivity.kt
@@ -1,10 +1,10 @@
 package hous.release.android.presentation.splash
 
+import android.animation.Animator
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.ActivityIntroBinding
@@ -16,8 +16,6 @@ import hous.release.android.util.binding.BindingActivity
 import hous.release.domain.entity.SplashState
 import hous.release.domain.usecase.GetSplashStateUseCase
 import javax.inject.Inject
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class IntroActivity : BindingActivity<ActivityIntroBinding>(R.layout.activity_intro) {
@@ -27,17 +25,22 @@ class IntroActivity : BindingActivity<ActivityIntroBinding>(R.layout.activity_in
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lottieSplashImg.playAnimation()
-        lifecycleScope.launch {
-            delay(3000)
-            when (getSplashStateUseCase()) {
-                SplashState.TUTORIAL -> startActivity<TutorialActivity>()
-                SplashState.LOGIN -> startActivity<LoginActivity>()
-                SplashState.ENTER_ROOM -> startActivity<EnterRoomActivity>()
-                SplashState.MAIN -> startActivity<MainActivity>()
+        binding.lottieSplashImg.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationEnd(animation: Animator?) {
+                when (getSplashStateUseCase()) {
+                    SplashState.TUTORIAL -> startActivity<TutorialActivity>()
+                    SplashState.LOGIN -> startActivity<LoginActivity>()
+                    SplashState.ENTER_ROOM -> startActivity<EnterRoomActivity>()
+                    SplashState.MAIN -> startActivity<MainActivity>()
+                }
+                overridePendingTransition(0, 0)
+                finish()
             }
-            overridePendingTransition(0, 0)
-            finish()
-        }
+
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationCancel(animation: Animator?) {}
+            override fun onAnimationRepeat(animation: Animator?) {}
+        })
     }
 
     inline fun <reified T : Activity> Context.startActivity(block: Intent.() -> Unit = {}) {
