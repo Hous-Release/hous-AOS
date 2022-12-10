5150dc3c406abdd04eda8aa59ed6f5b2dbe6dba5
KWY <wodrk1001@gmail.com>
2022-12-11 02:52:11 +0900

    #166 [fix] ktlint 수정

--- app/src/main/java/hous/release/android/util/component/PersonalityResultImage.kt
+++ app/src/main/java/hous/release/android/util/component/PersonalityResultImage.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
