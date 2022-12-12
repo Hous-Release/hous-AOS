037a45e01668b10c37b63304a0afed6f8438c84b
KWY <wodrk1001@gmail.com>
2022-12-12 23:07:48 +0900

    #171 [feat] 퇴사하기 버튼 클릭

--- app/src/main/java/hous/release/android/presentation/withdraw/WithdrawActivity.kt
+++ app/src/main/java/hous/release/android/presentation/withdraw/WithdrawActivity.kt
@@ -10,6 +10,9 @@ import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.ActivityWithdrawBinding
 import hous.release.android.presentation.login.LoginActivity
+import hous.release.android.util.HousLogEvent
+import hous.release.android.util.HousLogEvent.CLICK_WITHDRAW
+import hous.release.android.util.HousLogEvent.clickDateLogEvent
 import hous.release.android.util.ToastMessageUtil
 import hous.release.android.util.UiEvent
 import hous.release.android.util.binding.BindingActivity
@@ -48,6 +51,7 @@ class WithdrawActivity :
                             this@WithdrawActivity,
                             getString(R.string.withdraw_toast)
                         )
+                        clickDateLogEvent(CLICK_WITHDRAW)
                         startActivity(
                             Intent(this, LoginActivity::class.java).apply {
                                 addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
