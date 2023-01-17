55d632bba22410605511ef910e81f7fa0f6bf222
이빵주 <yungju9982@naver.com>
2023-01-18 00:14:30 +0900

    #186 [fix] 라이브데이터에서 stateFlow로 수정

--- app/src/main/java/hous/release/android/presentation/tutorial/TutorialViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/tutorial/TutorialViewModel.kt
@@ -1,12 +1,12 @@
 package hous.release.android.presentation.tutorial
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hous.release.domain.entity.SplashState
 import hous.release.domain.usecase.SetSplashStateUseCase
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,7 +15,7 @@ import javax.inject.Inject
 class TutorialViewModel @Inject constructor(
     private val setSplashStateUseCase: SetSplashStateUseCase
 ) : ViewModel() {
-    val showNextBtn = MutableLiveData<Boolean>()
+    val isNextBtn = MutableStateFlow(false)
 
     private val _isTutorialState = MutableSharedFlow<Boolean>()
     val isTutorialState = _isTutorialState.asSharedFlow()
