78a1463c5f4bf0a85cac3cd246099909637d3fe9
KWY <wodrk1001@gmail.com>
2023-05-04 15:48:26 +0900

    #205 [feat] design system 모듈 compile 버전 수정

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "hous.release.designsystem"
-    compileSdk = 33
+    compileSdk = AppConfig.compileSdkVersion
 
     defaultConfig {
-        minSdk = 24
-        targetSdk = 33
+        minSdk = AppConfig.minSdkVersion
+        targetSdk = AppConfig.targetSdkVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -34,11 +34,4 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
