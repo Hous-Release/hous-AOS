2ff69ebfe875e57f7de82f339543ebdb076737f1
KWY <wodrk1001@gmail.com>
2023-05-20 10:52:00 +0900

    #211 [chore] private function 주석 추가

--- testing/src/main/java/hous/release/testing/PrivateFunctionReflection.kt
+++ testing/src/main/java/hous/release/testing/PrivateFunctionReflection.kt
@@ -4,6 +4,11 @@ import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.isAccessible
 
+/*
+* [Class].callPrivateFunc(`private 메서드명`)
+* ex ) userInputViewModel.callPrivateFunc("selectedHashtags")
+* 
+* */
 inline fun <reified T> T.callPrivateFunc(name: String, vararg args: Any?): Any? =
     T::class
         .declaredMemberFunctions
@@ -11,6 +16,11 @@ inline fun <reified T> T.callPrivateFunc(name: String, vararg args: Any?): Any? 
         ?.apply { isAccessible = true }
         ?.call(this, *args)
 
+/*
+* [Class].getPrivateProperty<Class, Type>("private property name")
+* ex) profileEditViewModel.getPrivateProperty<ProfileEditViewModel, MutableStateFlow<User>>("cacheUser")
+*
+* */
 inline fun <reified T : Any, R> T.getPrivateProperty(name: String): R? =
     T::class
         .memberProperties
