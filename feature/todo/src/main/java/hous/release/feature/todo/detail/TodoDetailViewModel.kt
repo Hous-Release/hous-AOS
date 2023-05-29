f92492e4bb53d718febd5f90f23d7f71132b092e
KWY <wodrk1001@gmail.com>
2023-05-29 16:41:35 +0900

    #215 [feat] 호미선택 기능 구현

--- feature/todo/src/main/java/hous/release/feature/todo/detail/TodoDetailViewModel.kt
+++ feature/todo/src/main/java/hous/release/feature/todo/detail/TodoDetailViewModel.kt
@@ -1,20 +1,59 @@
 package hous.release.feature.todo.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hous.release.domain.entity.HomyType
+import hous.release.domain.usecase.todo.GetHomiesUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TodoDetailViewModel @Inject constructor() : ViewModel() {
-    val _isSelectedDay: MutableStateFlow<List<Boolean>> =
+class TodoDetailViewModel @Inject constructor(
+    private val getHomiesUseCase: GetHomiesUseCase
+) : ViewModel() {
+    private val _isSelectedDay: MutableStateFlow<List<Boolean>> =
         MutableStateFlow(listOf(false, false, false, false, false, false, false))
+    private val _homies: MutableStateFlow<List<SelectableHomy>> = MutableStateFlow(emptyList())
+
     val isSelectedDay: StateFlow<List<Boolean>> = _isSelectedDay.asStateFlow()
+    val homies = _homies.asStateFlow()
+
+    init {
+        getHomies()
+    }
+
+    private fun getHomies() {
+        viewModelScope.launch {
+            _homies.value = getHomiesUseCase().map { homy ->
+                SelectableHomy(
+                    id = homy.id,
+                    name = homy.name,
+                    homyType = homy.homyType
+                )
+            }
+        }
+    }
 
     fun selectDay(index: Int) {
         _isSelectedDay.value = _isSelectedDay.value
-            .mapIndexed { idx, b -> if (idx == index) !b else b }
+            .mapIndexed { idx, isSelected -> if (idx == index) !isSelected else isSelected }
+    }
+
+    fun selectHomy(index: Int) {
+        _homies.value = _homies.value.mapIndexed { idx, selectableHomy ->
+            if (idx == index) selectableHomy.copy(isSelected = !selectableHomy.isSelected)
+            else selectableHomy
+        }
     }
 }
+
+data class SelectableHomy(
+    val id: Int,
+    val isSelected: Boolean = false,
+    val name: String,
+    val homyType: HomyType
+)
--- feature/todo/src/test/java/hous/release/feature/todo/detail/TodoDetailViewModelTest.kt
+++ feature/todo/src/test/java/hous/release/feature/todo/detail/TodoDetailViewModelTest.kt
@@ -1,22 +1,31 @@
 package hous.release.feature.todo.detail
 
 import com.google.common.truth.Truth.assertThat
+import hous.release.domain.entity.HomyType
+import hous.release.domain.entity.todo.Homy
+import hous.release.domain.usecase.todo.GetHomiesUseCase
 import hous.release.testing.CoroutinesTestExtension
+import hous.release.testing.callPrivateFunc
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.assertIs
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @ExtendWith(CoroutinesTestExtension::class)
 class TodoDetailViewModelTest {
     private lateinit var todoDetailViewModel: TodoDetailViewModel
+    private val getHomiesUseCase: GetHomiesUseCase = mockk(relaxed = true)
 
     @BeforeEach
     fun setUp() {
-        todoDetailViewModel = TodoDetailViewModel()
+        todoDetailViewModel = TodoDetailViewModel(getHomiesUseCase)
     }
 
     @Test
@@ -36,6 +45,56 @@ class TodoDetailViewModelTest {
             { assertThat(todoDetailViewModel.isSelectedDay.value[0]).isEqualTo(true) },
             { assertThat(todoDetailViewModel.isSelectedDay.value[1]).isEqualTo(true) },
             { assertThat(todoDetailViewModel.isSelectedDay.value[2]).isEqualTo(true) }
+        )
+    }
+
+    @Test
+    @DisplayName("SelectableHomy 타입으로 매핑한다.")
+    fun homyChipClickTest() = runTest {
+        coEvery { getHomiesUseCase() } returns listOf(
+            Homy(0, "KWY", HomyType.BLUE),
+            Homy(1, "SYJ", HomyType.BLUE),
+            Homy(2, "LJW", HomyType.BLUE),
+            Homy(3, "LYJ", HomyType.BLUE),
+        )
+
+        todoDetailViewModel.callPrivateFunc("getHomies")
+
+        assertAll(
+            { assertIs<SelectableHomy>(todoDetailViewModel.homies.value[0]) },
+            { assertIs<SelectableHomy>(todoDetailViewModel.homies.value[1]) },
+            { assertIs<SelectableHomy>(todoDetailViewModel.homies.value[2]) },
+            { assertIs<SelectableHomy>(todoDetailViewModel.homies.value[3]) }
+        )
+    }
+
+    @Test
+    @DisplayName("호미를 복수로 클릭할 수 있다.")
+    fun homyChipClickTest2() = runTest {
+        coEvery { getHomiesUseCase() } returns listOf(
+            Homy(0, "KWY", HomyType.BLUE),
+            Homy(1, "SYJ", HomyType.BLUE),
+            Homy(2, "LJW", HomyType.BLUE),
+            Homy(3, "LYJ", HomyType.BLUE),
+        )
+        todoDetailViewModel.callPrivateFunc("getHomies")
+
+        assertAll(
+            { assertThat(todoDetailViewModel.homies.value[0].isSelected).isEqualTo(false) },
+            { assertThat(todoDetailViewModel.homies.value[1].isSelected).isEqualTo(false) },
+            { assertThat(todoDetailViewModel.homies.value[2].isSelected).isEqualTo(false) },
+            { assertThat(todoDetailViewModel.homies.value[3].isSelected).isEqualTo(false) }
+        )
+
+        todoDetailViewModel.selectHomy(0)
+        todoDetailViewModel.selectHomy(1)
+        todoDetailViewModel.selectHomy(2)
+
+        assertAll(
+            { assertThat(todoDetailViewModel.homies.value[0].isSelected).isEqualTo(true) },
+            { assertThat(todoDetailViewModel.homies.value[1].isSelected).isEqualTo(true) },
+            { assertThat(todoDetailViewModel.homies.value[2].isSelected).isEqualTo(true) },
+            { assertThat(todoDetailViewModel.homies.value[3].isSelected).isEqualTo(false) }
         )
     }
 }
