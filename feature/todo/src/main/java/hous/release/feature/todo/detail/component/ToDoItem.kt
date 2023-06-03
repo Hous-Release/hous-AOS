fa25f3b5dbad394cca5f88fd445afe322f40be33
KWY <wodrk1001@gmail.com>
2023-06-04 02:33:29 +0900

    #217 [chore] ktlint 적용

---
+++ feature/todo/src/main/java/hous/release/feature/todo/detail/component/ToDoItem.kt
@@ -0,0 +1,9 @@
+package hous.release.feature.todo.detail.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ToDoItem(
+    showToDoDetailBottomSheet: () -> Unit
+) {
+}
---
+++ feature/todo/src/main/java/hous/release/feature/todo/detail/component/TodoFilter.kt
@@ -0,0 +1,10 @@
+package hous.release.feature.todo.detail.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TodoFilter(
+    showFilterBottomSheet: () -> Unit
+) {
+
+}
