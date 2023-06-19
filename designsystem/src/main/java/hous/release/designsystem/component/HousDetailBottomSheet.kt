4c3bc7e3c2f16c6493b04b0ef81b4668127f9f48
KWY <wodrk1001@gmail.com>
2023-06-19 16:00:24 +0900

    #224 [chore] ktlint 수정

--- designsystem/src/main/java/hous/release/designsystem/component/HousDetailBottomSheet.kt
+++ designsystem/src/main/java/hous/release/designsystem/component/HousDetailBottomSheet.kt
@@ -1,6 +1,5 @@
 package hous.release.designsystem.component
 
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
