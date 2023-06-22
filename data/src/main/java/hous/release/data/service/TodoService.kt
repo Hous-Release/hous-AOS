e4f346fd6badac003daee4199b0a2891aa633834
KWY <wodrk1001@gmail.com>
2023-06-22 17:17:46 +0900

    #238 [fix] 라우터 수정

--- data/src/main/java/hous/release/data/service/TodoService.kt
+++ data/src/main/java/hous/release/data/service/TodoService.kt
@@ -16,7 +16,7 @@ import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface TodoService {
-    @GET("/v1/todos")
+    @GET("/v1/todos/main")
     suspend fun getTodoMainContent(): BaseResponse<ToDoMainResponse>
 
     @POST("/v1/todo/{todoId}/check")
