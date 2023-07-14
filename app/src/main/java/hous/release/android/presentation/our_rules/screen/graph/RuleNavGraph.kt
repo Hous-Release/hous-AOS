33c268e18a1ec363fdbb19729f780461b60af2ee
murjune <sis9221302@gmail.com>
2023-07-14 15:57:06 +0900

    #263 [ui] RuleNavGraph

---
+++ app/src/main/java/hous/release/android/presentation/our_rules/screen/graph/RuleNavGraph.kt
@@ -0,0 +1,61 @@
+package hous.release.android.presentation.our_rules.screen.graph
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import hous.release.android.presentation.our_rules.screen.RulesScreens
+
+@Composable
+fun RuleNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RulesScreens.Main.route,
+        route = RulesScreens.ROUTE
+    ) {
+        mainRuleScreen(
+            onNavigateToAddRule = navController::navigateToAddRule,
+            onNavigateToDetailRule = navController::navigateToDetailRule
+        )
+        addRuleScreen()
+        ruleDetailsGraph(
+            onNavigateToUpdateRule = navController::navigateUpdateRule,
+            onNavigateToDeleteRule = navController::navigateDeleteRule
+        )
+    }
+}
+
+// Screens
+
+private fun NavGraphBuilder.mainRuleScreen(
+    onNavigateToAddRule: () -> Unit,
+    onNavigateToDetailRule: (Int) -> Unit
+) {
+    composable(RulesScreens.Main.route) {}
+}
+
+private fun NavGraphBuilder.addRuleScreen() {
+    composable(RulesScreens.ADD.route) {}
+}
+
+// Navigation
+
+fun NavController.navigateToAddRule() {
+    navigate(RulesScreens.ADD.route)
+}
+
+fun NavController.navigateToDetailRule(id: Int) {
+    navigate(RulesScreens.Detail.createRouteBy(id))
+}
+
+fun NavController.navigateUpdateRule(id: Int) {
+    navigate(RulesScreens.Update.createRouteBy(id))
+}
+
+fun NavController.navigateDeleteRule(id: Int) {
+    navigate(RulesScreens.Delete.createRouteBy(id))
+}
