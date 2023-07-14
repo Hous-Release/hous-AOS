6166159e255fa8cf482cbba20f23d21d7d83239f
murjune <sis9221302@gmail.com>
2023-07-14 17:07:40 +0900

    #263 [feat] arguments(navArguments), createRouteBy 함수 추가

--- app/src/main/java/hous/release/android/presentation/our_rules/screen/RulesScreens.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/screen/RulesScreens.kt
@@ -1,23 +1,30 @@
 package hous.release.android.presentation.our_rules.screen
 
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
 sealed class RulesScreens(val route: String) {
     object Main : RulesScreens("main")
     object ADD : RulesScreens("add")
-    object Delete : RulesScreens("delete/{$RULE_ID_KEY}}") {
-        fun createRouteBy(id: Int): String = "detail/$id"
+    object Delete : RulesScreens("delete/{$RULE_ID_KEY}") {
+        val arguments = listOf(navArgument(RULE_ID_KEY) { type = NavType.IntType })
+        fun createRouteBy(id: Int): String = "delete/$id"
     }
 
-    object Update : RulesScreens("update/{$RULE_ID_KEY}}") {
-        fun createRouteBy(id: Int): String = "detail/$id"
+    object Update : RulesScreens("update/{$RULE_ID_KEY}") {
+        val arguments = listOf(navArgument(RULE_ID_KEY) { type = NavType.IntType })
+        fun createRouteBy(id: Int): String = "update/$id"
     }
 
-    object Detail : RulesScreens("detail/{$RULE_ID_KEY}}") {
+    object Detail : RulesScreens("detail/{$RULE_ID_KEY}") {
+        val arguments = listOf(navArgument(RULE_ID_KEY) { type = NavType.IntType })
+
         fun createRouteBy(id: Int): String = "detail/$id"
     }
 
     companion object {
         const val ROUTE = "rules_route"
         const val DETAIL_ROUTE = "rules_detail_route"
-        private const val RULE_ID_KEY = "id"
+        const val RULE_ID_KEY = "id"
     }
 }
