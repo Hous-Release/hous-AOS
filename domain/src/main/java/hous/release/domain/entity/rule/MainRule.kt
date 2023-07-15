f41fdb8911359cdc561e975205d63acb136f4e56
murjune <sis9221302@gmail.com>
2023-07-15 23:03:16 +0900

    #265 [fix] domain model propery 변경

--- domain/src/main/java/hous/release/domain/entity/rule/MainRule.kt
+++ domain/src/main/java/hous/release/domain/entity/rule/MainRule.kt
@@ -1,11 +1,10 @@
 package hous.release.domain.entity.rule
 
-import hous.release.domain.entity.rule.type.RuleType
-
 data class MainRule(
     val id: Int = NO_ID,
     val name: String = NO_NAME,
-    val ruleType: RuleType = RuleType.GENERAL
+    val createdAt: String = "",
+    val isNew: Boolean = false
 ) {
     companion object {
         private const val NO_NAME = "다른 Rule도 추가해보세요!"
