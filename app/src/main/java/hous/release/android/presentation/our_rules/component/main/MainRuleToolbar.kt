7e7d44a3aae1b725c6cf33ee298597173ed94b09
murjune <sis9221302@gmail.com>
2023-07-23 04:16:56 +0900

    #265 [feat] MainRuleToolbar

--- app/src/main/java/hous/release/android/presentation/our_rules/component/main/MainRuleToolbar.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/component/main/MainRuleToolbar.kt
@@ -1,53 +1,69 @@
 package hous.release.android.presentation.our_rules.component.main
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hous.release.designsystem.R
 import hous.release.designsystem.component.HousToolbarSlot
 import hous.release.designsystem.theme.HousG4
 import hous.release.designsystem.theme.HousTheme
-import hous.release.android.R.drawable as app
-import hous.release.designsystem.R.drawable as feature
 
 @Composable
 fun MainRuleToolbar(
-    onBack: () -> Unit = {},
     onClickSetting: () -> Unit = {},
-    title: String = "우리 집 Rules"
+    modifier: Modifier = Modifier,
+    title: String = "우리 집 Rules",
+    onBack: () -> Unit = { }
 ) {
-    HousToolbarSlot(
+    var expanded by remember { mutableStateOf(false) }
+    val onDismiss = { expanded = false }
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 20.dp, bottom = 24.dp, end = 8.dp),
-        leadingIcon = {
-            Icon(
-                modifier = Modifier.clickable {
-                    onBack()
-                },
-                tint = HousG4,
-                painter = painterResource(id = feature.ic_back),
-                contentDescription = null
-            )
-        },
-        title = title,
-        trailingIcon = {
-            Icon(
-                modifier = Modifier.clickable {
-                    onClickSetting()
-                },
-                tint = HousG4,
-                painter = painterResource(id = app.ic_our_rules_setting),
-                contentDescription = null
-            )
-        }
-    )
+            .wrapContentSize(align = Alignment.TopEnd)
+    ) {
+        HousToolbarSlot(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 24.dp, end = 8.dp),
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        onBack()
+                    },
+                    tint = HousG4,
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = null
+                )
+            },
+            title = title,
+            trailingIcon = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        expanded = true
+                    },
+                    tint = HousG4,
+                    painter = painterResource(id = hous.release.android.R.drawable.ic_our_rules_setting),
+                    contentDescription = null
+                )
+            }
+        )
+        MainRuleDropDownMenu(expanded, onDismiss)
+    }
 }
 
 @Preview
