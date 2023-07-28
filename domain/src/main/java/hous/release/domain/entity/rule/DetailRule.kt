37e2768ca8edf656cd4112217384969388b083d4
murjune <sis9221302@gmail.com>
2023-07-28 11:59:06 +0900

    #275 [fix] DetailRule dto 수정

--- domain/src/main/java/hous/release/domain/entity/rule/DetailRule.kt
+++ domain/src/main/java/hous/release/domain/entity/rule/DetailRule.kt
@@ -2,7 +2,7 @@ package hous.release.domain.entity.rule
 
 import hous.release.domain.entity.Rule
 
-class DetailRule(
+data class DetailRule(
     override val id: Int = NO_ID,
     override val name: String = NO_NAME,
     val description: String = NO_DESCRIPTION,
