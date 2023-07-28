cf3f00f6a15eba0ba0d4c445bcfee08b006f6dcc
murjune <sis9221302@gmail.com>
2023-07-28 12:58:49 +0900

    #269 [feat/data] rule 추가 data 로직

--- data/src/main/java/hous/release/data/datasource/RuleDataSource.kt
+++ data/src/main/java/hous/release/data/datasource/RuleDataSource.kt
@@ -1,13 +1,15 @@
 package hous.release.data.datasource
 
-import hous.release.data.entity.request.AddRulesRequest
 import hous.release.data.entity.request.DeleteRulesRequest
 import hous.release.data.entity.request.EditRulesRequest
+import hous.release.data.entity.request.rule.AddRulesRequest
 import hous.release.data.entity.response.NoDataResponse
 import hous.release.data.entity.response.rule.DetailRuleResponse
 import hous.release.data.entity.response.rule.MainRuleResponse
 import hous.release.data.entity.response.rule.MainRulesResponse
 import hous.release.data.service.RuleService
+import hous.release.data.util.toImagePart
+import hous.release.data.util.toPlainTextRequestBody
 import hous.release.domain.entity.rule.MainRule
 import javax.inject.Inject
 
@@ -18,8 +20,13 @@ class RuleDataSource @Inject constructor(private val ruleService: RuleService) {
 
     suspend fun fetchDetailRuleBy(id: Int): DetailRuleResponse =
         ruleService.getDetailRuleBy(id).data
-    suspend fun postAddedRuleContent(addedRules: List<String>): Result<NoDataResponse> =
-        runCatching { ruleService.postAddedRuleContent(AddRulesRequest(addedRules = addedRules)) }
+
+    suspend fun postAddedRuleContent(req: AddRulesRequest): NoDataResponse =
+        ruleService.postNewRule(
+            description = req.description.toPlainTextRequestBody(),
+            name = req.name.toPlainTextRequestBody(),
+            images = req.imageFiles.map { file -> file.toImagePart("images") }
+        )
 
     suspend fun putEditedRuleContent(editedRules: List<MainRule>): NoDataResponse =
         ruleService.putEditedRuleContent(
--- domain/src/main/java/hous/release/domain/repository/RuleRepository.kt
+++ domain/src/main/java/hous/release/domain/repository/RuleRepository.kt
@@ -2,13 +2,20 @@ package hous.release.domain.repository
 
 import hous.release.domain.entity.rule.DetailRule
 import hous.release.domain.entity.rule.MainRule
+import hous.release.domain.enums.PhotoUri
 import hous.release.domain.util.ApiResult
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 
 interface RuleRepository {
     suspend fun fetchMainRules(): List<MainRule>
     suspend fun fetchDetailRule(id: Int): DetailRule
-    suspend fun postAddedRule(addedRuleList: List<String>): Int
+    suspend fun postAddedRule(
+        description: String,
+        name: String,
+        imageUri: List<File>
+    )
+
     fun putEditedRuleContent(editedRuleList: List<MainRule>): Flow<ApiResult<String>>
     fun deleteRuleContent(deleteRules: List<Int>): Flow<ApiResult<String>>
 }
