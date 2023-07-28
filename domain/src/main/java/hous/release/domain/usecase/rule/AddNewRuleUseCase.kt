7dede7a4a1c55d67cd618b0fb944a6217fbbf125
murjune <sis9221302@gmail.com>
2023-07-28 12:59:10 +0900

    #269 [feat/domain] AddNewRuleUsecase

---
+++ domain/src/main/java/hous/release/domain/usecase/rule/AddNewRuleUseCase.kt
@@ -0,0 +1,21 @@
+package hous.release.domain.usecase.rule
+
+import hous.release.domain.enums.PhotoUri
+import hous.release.domain.repository.PhotoRepository
+import hous.release.domain.repository.RuleRepository
+import javax.inject.Inject
+
+class AddNewRuleUseCase @Inject constructor(
+    private val photoRepository: PhotoRepository,
+    private val ourRulesRepository: RuleRepository
+) {
+    suspend operator fun invoke(
+        description: String,
+        name: String,
+        imageUri: List<PhotoUri>
+    ): Boolean {
+        val localFiles = photoRepository.fetchPhotos(imageUri)
+        ourRulesRepository.postAddedRule(description, name, localFiles)
+        return photoRepository.removeTemporayPhotos()
+    }
+}
--- domain/src/main/java/hous/release/domain/usecase/rule/PostAddRulesUseCase.kt
+++
@@ -1,9 +0,0 @@
-package hous.release.domain.usecase.rule
-
-import hous.release.domain.repository.RuleRepository
-import javax.inject.Inject
-
-class PostAddRulesUseCase @Inject constructor(private val ourRulesRepository: RuleRepository) {
-    suspend operator fun invoke(addedRuleList: List<String>) =
-        ourRulesRepository.postAddedRule(addedRuleList)
-}
