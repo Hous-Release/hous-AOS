f555844a6719c57297a6c359f4b15b860a6528c9
murjune <sis9221302@gmail.com>
2023-07-31 12:57:36 +0900

    #270 [ui] MainRuleViewModel 수정

--- app/src/main/java/hous/release/android/presentation/our_rules/viewmodel/MainRuleViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/viewmodel/MainRuleViewModel.kt
@@ -3,15 +3,16 @@ package hous.release.android.presentation.our_rules.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import hous.release.android.presentation.our_rules.event.MainRuleReducer
+import hous.release.android.di.MainRules
 import hous.release.android.presentation.our_rules.model.DetailRuleUiModel
-import hous.release.android.presentation.our_rules.model.PhotoUiModel
+import hous.release.android.util.event.Reducer
 import hous.release.data.repository.RulePhotoRepository
 import hous.release.domain.entity.rule.DetailRule
 import hous.release.domain.entity.rule.MainRule
 import hous.release.domain.enums.PhotoUri
 import hous.release.domain.usecase.rule.GetDetailRuleUseCase
 import hous.release.domain.usecase.rule.GetMainRulesUseCase
+import hous.release.domain.usecase.search.SearchRuleUseCase
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collectLatest
@@ -27,7 +28,8 @@ class MainRuleViewModel @Inject constructor(
     private val photoSaver: RulePhotoRepository,
     private val getMainRulesUseCase: GetMainRulesUseCase,
     private val getDetailRuleUseCase: GetDetailRuleUseCase,
-    private val reducer: MainRuleReducer
+    private val searcher: SearchRuleUseCase,
+    @MainRules private val reducer: Reducer<MainRulesState, MainRulesEvent>
 ) : ViewModel() {
 
     private val uiEvents = Channel<MainRulesEvent>()
@@ -53,7 +55,12 @@ class MainRuleViewModel @Inject constructor(
 
     fun searchRule(searchQuery: String) {
         viewModelScope.launch {
-            uiEvents.send(MainRulesEvent.SearchRule(searchQuery))
+            uiEvents.send(
+                MainRulesEvent.SearchRule(
+                    searchQuery,
+                    searcher(searchQuery, uiState.value.originRules)
+                )
+            )
         }
     }
 
@@ -73,24 +80,6 @@ class MainRuleViewModel @Inject constructor(
                 }
         }
     }
-
-    private fun DetailRule.toUiModel() = DetailRuleUiModel(
-        id = id,
-        name = name,
-        description = description,
-        images = images.map { url ->
-            PhotoUiModel(
-                url = url,
-                isUploading = true
-            )
-        },
-        updatedAt = updatedAt
-    )
-
-    private companion object {
-        const val TEST_IMAGE_URL =
-            "https://github.com/Hous-Release/hous-AOS/assets/87055456/e5aa4a25-cbde-4b13-ab3c-6cc002dbc00f"
-    }
 }
 
 sealed class MainRulesEvent {
@@ -99,7 +88,9 @@ sealed class MainRulesEvent {
     data class Refresh(val rules: List<MainRule>) : MainRulesEvent()
     data class FetchMainRules(val rules: List<MainRule>) : MainRulesEvent()
     data class FetchDetailRule(val rule: DetailRule) : MainRulesEvent()
-    data class SearchRule(val searchQuery: String) : MainRulesEvent()
+    data class SearchRule(val searchQuery: String, val filteredRules: List<MainRule>) :
+        MainRulesEvent()
+
     object DeleteAllFile : MainRulesEvent()
 }
 
