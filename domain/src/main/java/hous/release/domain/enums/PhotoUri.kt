76b707e53e0e5a33a88bb9c68852d43ff517baf9
murjune <sis9221302@gmail.com>
2023-07-31 14:37:44 +0900

    #270 [fix] PhotoUri

--- domain/src/main/java/hous/release/domain/enums/PhotoUri.kt
+++ domain/src/main/java/hous/release/domain/enums/PhotoUri.kt
@@ -1,4 +1,12 @@
 package hous.release.domain.enums
 
 @JvmInline
-value class PhotoUri(val path: String)
+value class PhotoUri(val path: String) {
+    init {
+        require(path.isNotBlank() && path.substringBefore(":") == SCHEME)
+    }
+
+    private companion object {
+        const val SCHEME = "content"
+    }
+}
