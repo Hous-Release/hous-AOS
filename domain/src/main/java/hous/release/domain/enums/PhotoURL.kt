896becaefcd596a5fb04a9390d3efd5b18137472
murjune <sis9221302@gmail.com>
2023-07-31 14:37:50 +0900

    #270 [add] PhotoURL

--- domain/src/main/java/hous/release/domain/enums/PhotoURL.kt
+++ domain/src/main/java/hous/release/domain/enums/PhotoURL.kt
@@ -1,4 +1,8 @@
 package hous.release.domain.enums
 
 @JvmInline
-value class PhotoURL(val path: String)
+value class PhotoURL(val path: String) {
+    init {
+        require(path.isNotBlank())
+    }
+}
