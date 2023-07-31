b6e65234e597303aab9882d5f19f8048d9e6b783
이빵주 <yungju9982@naver.com>
2023-08-01 07:58:56 +0900

    #281 [ui] 회원탈퇴 뷰 구현

--- app/src/main/java/hous/release/android/presentation/withdraw/WithdrawFragment.kt
+++ app/src/main/java/hous/release/android/presentation/withdraw/WithdrawFragment.kt
@@ -2,22 +2,34 @@ package hous.release.android.presentation.withdraw
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentWithdrawBinding
 import hous.release.android.util.binding.BindingFragment
+import hous.release.android.util.extension.setOnSingleClickListener
 
 @AndroidEntryPoint
 class WithdrawFragment : BindingFragment<FragmentWithdrawBinding>(R.layout.fragment_withdraw) {
+    private val withdrawViewModel by activityViewModels<WithdrawViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = withdrawViewModel
+        initWithdrawDoneClickListener()
+        initBackBtnClickListener()
+    }
 
-        binding.next.setOnClickListener {
+    /** TODO 영주 : 클릭리스너 없애고 데바로 탈퇴하기 api 연결 후 옵저버로 넘기는거 구현 */
+    private fun initWithdrawDoneClickListener() {
+        binding.tvWithdrawDone.setOnSingleClickListener {
             findNavController().navigate(R.id.action_withdrawFragment_to_feedbackFragment)
         }
+    }
 
-        binding.back.setOnClickListener {
+    private fun initBackBtnClickListener() {
+        binding.btnWithdrawBack.setOnClickListener {
             requireActivity().finish()
         }
     }
