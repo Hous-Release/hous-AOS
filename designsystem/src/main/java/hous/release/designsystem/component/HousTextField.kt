21eccfc606d6d8159e30beeb6851171eb4ec05e1
KWY <wodrk1001@gmail.com>
2023-08-05 16:27:19 +0900

    #279 [refactor] hous text field 리팩토링

--- designsystem/src/main/java/hous/release/designsystem/component/HousTextField.kt
+++ designsystem/src/main/java/hous/release/designsystem/component/HousTextField.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalFocusManager
@@ -35,7 +36,6 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import hous.release.designsystem.R
 import hous.release.designsystem.theme.HousBlue
@@ -44,43 +44,9 @@ import hous.release.designsystem.theme.HousG1
 import hous.release.designsystem.theme.HousG5
 import hous.release.designsystem.theme.HousTheme
 
-@Composable
-fun HousBottomLimitedTextField(
-    modifier: Modifier = Modifier,
-    text: String,
-    color: Color,
-    limitTextCount: Int,
-    height: Dp,
-    onTextChange: (String) -> Unit
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(height)
-            .clip(RoundedCornerShape(8.dp))
-            .background(color)
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-    ) {
-        HousTextField(
-            modifier = Modifier
-                .height(IntrinsicSize.Min)
-                .weight(1f),
-            text = text,
-            onTextChange = { innerText ->
-                if (innerText.length <= limitTextCount) onTextChange(innerText)
-            }
-        )
-        Text(
-            modifier = Modifier
-                .wrapContentSize()
-                .fillMaxWidth(),
-            text = "${text.length}/$limitTextCount",
-            textAlign = TextAlign.End,
-            style = HousTheme.typography.en2,
-            color = HousG5
-        )
-    }
-}
+const val SEARCH_TEXT_FIELD = 0
+const val RIGHT_LIMITED_TEXT_FIELD = 1
+const val BOTTOM_END_TEXT_FIELD = 2
 
 @Composable
 fun HousLeftLimitedTextField(
@@ -99,6 +65,7 @@ fun HousLeftLimitedTextField(
             .padding(horizontal = 16.dp, vertical = 11.dp)
     ) {
         HousTextField(
+            textFiledMode = RIGHT_LIMITED_TEXT_FIELD,
             modifier = Modifier
                 .height(IntrinsicSize.Min)
                 .weight(1f),
@@ -125,6 +92,10 @@ fun HousLeftLimitedTextField(
 }
 
 @Composable
+@Deprecated(
+    "Preview 를 보시고 HousTextField 함수를 사용하시면 됩니다.",
+    replaceWith = ReplaceWith("HousTextField")
+)
 fun HousSearchTextField(
     modifier: Modifier = Modifier,
     text: String,
@@ -133,6 +104,7 @@ fun HousSearchTextField(
     onTextChange: (String) -> Unit
 ) {
     HousTextField(
+        textFiledMode = SEARCH_TEXT_FIELD,
         modifier = modifier
             .fillMaxWidth()
             .height(IntrinsicSize.Min)
@@ -141,7 +113,6 @@ fun HousSearchTextField(
             .padding(horizontal = 16.dp, vertical = 11.dp),
         text = text,
         hint = hint,
-        isPrefixIcon = true,
         keyboardOptions = KeyboardOptions.Default.copy(
             capitalization = KeyboardCapitalization.Sentences,
             autoCorrect = true,
@@ -156,57 +127,141 @@ fun HousSearchTextField(
 }
 
 @Composable
-private fun HousTextField(
+fun HousTextField(
+    textFiledMode: Int,
     modifier: Modifier,
     text: String,
     hint: String = "",
-    isPrefixIcon: Boolean = false,
+    limitTextCount: Int = 20,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     onTextChange: (String) -> Unit
 ) {
+    var backgroundColor by remember { mutableStateOf(HousG1) }
     BasicTextField(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+            .clip(RoundedCornerShape(8.dp))
+            .onFocusChanged { focus ->
+                backgroundColor = if (focus.isFocused) HousBlueL2 else HousG1
+            }
+            .background(backgroundColor)
+            .padding(horizontal = 16.dp, vertical = 11.dp),
         value = text,
-        onValueChange = { onTextChange(it) },
+        onValueChange = { innerText ->
+            if (innerText.length <= limitTextCount) onTextChange(innerText)
+        },
         textStyle = HousTheme.typography.b2,
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
         cursorBrush = SolidColor(HousBlue)
     ) { innerTextField ->
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            if (isPrefixIcon) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_search),
-                    contentDescription = null
+        when (textFiledMode) {
+            SEARCH_TEXT_FIELD -> {
+                HousSearchTextField(
+                    text = text,
+                    hint = hint,
+                    innerTextField = innerTextField
                 )
             }
-            Box(
-                modifier = Modifier.fillMaxHeight(),
-                contentAlignment = if (isPrefixIcon) Alignment.CenterStart else Alignment.TopStart
-            ) {
-                if (text.isEmpty()) {
-                    Text(
-                        text = hint,
-                        color = HousG5,
-                        style = HousTheme.typography.b2
-                    )
-                }
-                innerTextField()
+            RIGHT_LIMITED_TEXT_FIELD -> {
+                HousRightLimitedTextField(
+                    text = text,
+                    limitTextCount = limitTextCount,
+                    innerTextField = innerTextField
+                )
+            }
+            BOTTOM_END_TEXT_FIELD -> {
+                HousBottomEndLimitedTextField(
+                    text = text,
+                    limitTextCount = limitTextCount,
+                    innerTextField = innerTextField
+                )
             }
         }
     }
 }
 
+@Composable
+private fun HousSearchTextField(
+    text: String,
+    hint: String,
+    innerTextField: @Composable () -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = null
+        )
+        Box(
+            modifier = Modifier.fillMaxHeight(),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            if (text.isEmpty()) {
+                Text(
+                    text = hint,
+                    color = HousG5,
+                    style = HousTheme.typography.b2
+                )
+            }
+            innerTextField()
+        }
+    }
+}
+
+@Composable
+private fun HousRightLimitedTextField(
+    text: String,
+    limitTextCount: Int,
+    innerTextField: @Composable () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        innerTextField()
+        Text(
+            text = "${text.length}/${limitTextCount}",
+            style = HousTheme.typography.en2,
+            color = HousG5
+        )
+    }
+}
+
+@Composable
+private fun HousBottomEndLimitedTextField(
+    text: String,
+    limitTextCount: Int,
+    innerTextField: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier.height(136.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        innerTextField()
+        Text(
+            modifier = Modifier
+                .wrapContentSize()
+                .fillMaxWidth(),
+            text = "${text.length}/${limitTextCount}",
+            textAlign = TextAlign.End,
+            style = HousTheme.typography.en2,
+            color = HousG5
+        )
+    }
+}
+
 @Preview(name = "검색 text field")
 @Composable
-private fun DescriptionTextFieldPreview() {
+private fun TextFieldPreview() {
     HousTheme {
         var text by remember { mutableStateOf("") }
         val onChange: (String) -> Unit = { text = it }
-        HousSearchTextField(
+        HousTextField(
+            textFiledMode = SEARCH_TEXT_FIELD,
             text = text,
             onTextChange = onChange,
             hint = "검색하기",
@@ -215,35 +270,33 @@ private fun DescriptionTextFieldPreview() {
     }
 }
 
-@Preview(name = "왼쪽에 제한이 있는 text field")
+@Preview(name = "우측에 제한이 있는 text field")
 @Composable
 private fun HousLeftLimitedTextFieldPreview() {
     HousTheme {
         var text by remember { mutableStateOf("") }
         val onChange: (String) -> Unit = { text = it }
-        HousLeftLimitedTextField(
+        HousTextField(
+            textFiledMode = RIGHT_LIMITED_TEXT_FIELD,
             modifier = Modifier,
             text = text,
             onTextChange = onChange,
-            color = HousG1,
-            limitTextCount = 20
         )
     }
 }
 
-@Preview(name = "아래쪽에 제한이 있는 text field")
+@Preview(name = "우측 아래에 제한이 있는 text field")
 @Composable
-private fun HousBottomLimitedTextFieldPreview() {
+private fun HousBottomEndLimitedTextFieldPreview() {
     HousTheme {
         var text by remember { mutableStateOf("") }
         val onChange: (String) -> Unit = { text = it }
-        HousBottomLimitedTextField(
+        HousTextField(
+            textFiledMode = BOTTOM_END_TEXT_FIELD,
+            limitTextCount = 50,
             modifier = Modifier,
             text = text,
             onTextChange = onChange,
-            color = HousBlueL2,
-            height = 136.dp,
-            limitTextCount = 50
         )
     }
 }
