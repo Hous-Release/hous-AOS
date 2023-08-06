4196405a0e527afc2edfec021ae97108eb2f99c8
이빵주 <yungju9982@naver.com>
2023-08-06 18:34:41 +0900

    #281 [feat] 탈퇴하기 완료 뷰에서 뒤로가기/완료 클릭시 종료로직 구현

--- app/src/main/java/hous/release/android/presentation/withdraw/WithdrawDoneFragment.kt
+++ app/src/main/java/hous/release/android/presentation/withdraw/WithdrawDoneFragment.kt
@@ -1,11 +1,16 @@
 package hous.release.android.presentation.withdraw
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.core.app.ActivityCompat
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentWithdrawDoneBinding
+import hous.release.android.presentation.login.LoginActivity
 import hous.release.android.util.binding.BindingFragment
+import hous.release.android.util.extension.setOnSingleClickListener
 
 @AndroidEntryPoint
 class WithdrawDoneFragment :
@@ -13,11 +18,25 @@ class WithdrawDoneFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initReturnBtnClickListener()
+        initBackPressedCallback()
     }
 
     private fun initReturnBtnClickListener() {
-        binding.tvWithdrawDoneReturn.setOnClickListener {
-            requireActivity().finish()
+        binding.tvWithdrawDoneReturn.setOnSingleClickListener {
+            startActivity(Intent(requireActivity(), LoginActivity::class.java))
+            requireActivity().finishAffinity()
         }
+    }
+
+    private fun initBackPressedCallback() {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                    ActivityCompat.finishAffinity(requireActivity())
+                }
+            }
+        )
     }
 }
