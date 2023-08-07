5a0ec7d2126bf11096856d3981d71046450456b0
murjune <sis9221302@gmail.com>
2023-08-07 22:42:40 +0900

    #270 [fix(domain)] PhtotoURL 적용

--- domain/src/main/java/hous/release/domain/entity/rule/DetailRule.kt
+++ domain/src/main/java/hous/release/domain/entity/rule/DetailRule.kt
@@ -1,12 +1,13 @@
 package hous.release.domain.entity.rule
 
 import hous.release.domain.entity.Rule
+import hous.release.domain.value.PhotoURL
 
 data class DetailRule(
     override val id: Int = NO_ID,
     override val name: String = NO_NAME,
     val description: String = NO_DESCRIPTION,
-    val images: List<String> = emptyList(),
+    val images: List<PhotoURL> = emptyList(),
     val updatedAt: String = ""
 ) : Rule(id, name) {
     companion object {
