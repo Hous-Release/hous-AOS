b7eb7c25f081b5d0620845408b6a43bcb4878937
murjune <sis9221302@gmail.com>
2023-08-08 23:26:31 +0900

    #293 [refactor] photo get하고 caching하는 방식 수정

--- domain/src/main/java/hous/release/domain/repository/PhotoRepository.kt
+++ domain/src/main/java/hous/release/domain/repository/PhotoRepository.kt
@@ -1,14 +1,12 @@
 package hous.release.domain.repository
 
-import hous.release.domain.value.PhotoURL
-import hous.release.domain.value.PhotoUri
+import hous.release.domain.entity.Photo
 import kotlinx.coroutines.flow.Flow
 import java.io.File
 
 interface PhotoRepository {
-    fun fetchRemotePhotosFlow(paths: List<PhotoURL>): Flow<List<PhotoUri?>>
-    suspend fun fetchPhotosByURL(urls: List<PhotoURL>): List<File>
-    suspend fun fetchPhotosByUri(uris: List<PhotoUri>): List<File>
+    fun fetchRemotePhotosFlow(paths: List<Photo>): Flow<List<Photo?>>
+    suspend fun fetchPhotosBy(uris: List<Photo>): List<File>
     suspend fun removePhoto(path: String): Boolean
     suspend fun removeTemporayPhotos(): Boolean
 }
