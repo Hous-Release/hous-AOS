88f2d04a805386e8479d9c833cdc7e47b03f7db4
이빵주 <yungju9982@naver.com>
2023-08-18 13:00:00 +0900

    #257 [feat] ImageDownloader 클래스 구현

---
+++ app/src/main/java/hous/release/android/util/ImageDownloader.kt
@@ -0,0 +1,52 @@
+package hous.release.android.util
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.OutputStream
+import java.net.URL
+
+class ImageDownloader(
+    private val context: Context
+) {
+    suspend fun downloadAndSaveImage(imageUrl: String, fileName: String) {
+        withContext(Dispatchers.IO) {
+            val bitmap = BitmapFactory.decodeStream(URL(imageUrl).openStream())
+            saveImageToGallery(bitmap, fileName)
+        }
+    }
+
+    private fun saveImageToGallery(bitmap: Bitmap, fileName: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                put(MediaStore.MediaColumns.IS_PENDING, 1)
+            }
+        }
+
+        val resolver = context.contentResolver
+        val uri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        uri?.let {
+            val os: OutputStream? = resolver.openOutputStream(it)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os)
+            os?.flush()
+            os?.close()
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                contentValues.clear()
+                contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+                resolver.update(uri, contentValues, null, null)
+            }
+        }
+    }
+}
