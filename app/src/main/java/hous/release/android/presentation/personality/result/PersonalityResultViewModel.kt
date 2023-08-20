f2a5f1c269ecc6946d1b681800c959ba4620cf6e
이빵주 <yungju9982@naver.com>
2023-08-20 13:00:00 +0900

    #257 [feat] 뷰모델에서 이미지 다운받는 로직 구현

--- app/src/main/java/hous/release/android/presentation/personality/result/PersonalityResultViewModel.kt
+++ app/src/main/java/hous/release/android/presentation/personality/result/PersonalityResultViewModel.kt
@@ -1,11 +1,16 @@
 package hous.release.android.presentation.personality.result
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hous.release.android.util.ImageDownloader
+import hous.release.android.util.UiEvent
 import hous.release.domain.entity.response.PersonalityResult
 import hous.release.domain.usecase.GetPersonalityResultUseCase
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -13,10 +18,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PersonalityResultViewModel @Inject constructor(
+    private val application: Application,
     private val getPersonalityResultUseCase: GetPersonalityResultUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(PersonalityResult())
     val uiState = _uiState.asStateFlow()
+
+    private val _uiEvent = MutableSharedFlow<UiEvent>()
+    val uiEvent = _uiEvent.asSharedFlow()
+
+    private val imageDownloader = ImageDownloader(application)
 
     fun getPersonalityResult(personalityColor: String) {
         viewModelScope.launch {
@@ -26,6 +37,26 @@ class PersonalityResultViewModel @Inject constructor(
                 }.onFailure {
                     Timber.e(it.message)
                 }
+        }
+    }
+
+    fun downloadImage() {
+        viewModelScope.launch {
+            _uiEvent.emit(UiEvent.LOADING)
+            try {
+                imageDownloader.downloadAndSaveImage(
+                    _uiState.value.firstDownloadImageUrl,
+                    _uiState.value.color.name + "1"
+                )
+                imageDownloader.downloadAndSaveImage(
+                    _uiState.value.secondDownloadImageUrl,
+                    _uiState.value.color.name + "2"
+                )
+                _uiEvent.emit(UiEvent.SUCCESS)
+            } catch (e: Exception) {
+                Timber.e(e.message)
+                _uiEvent.emit(UiEvent.ERROR)
+            }
         }
     }
 }
