f164e52a7e87eff9ab3986812118f51a08f411dd
murjune <sis9221302@gmail.com>
2023-09-11 01:35:49 +0900

    #313 [ui] DropDownMenu radius값 수정

--- app/src/main/java/hous/release/android/presentation/our_rules/component/main/MainRuleDropDownMenu.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/component/main/MainRuleDropDownMenu.kt
@@ -1,24 +1,27 @@
 package hous.release.android.presentation.our_rules.component.main
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -33,8 +36,8 @@ import hous.release.designsystem.theme.HousG6
 import hous.release.designsystem.theme.HousTheme
 
 private val MinMenuWidth = 139.dp
-private val DropdownMenuItemContentStartPadding = 20.dp
 private val DropdownDividerPadding = 14.dp
+private val LocalShapes = staticCompositionLocalOf { Shapes() }
 
 @Composable
 fun MainRuleDropDownMenu(
@@ -50,47 +53,62 @@ fun MainRuleDropDownMenu(
 
     val dropDownOffset = DpOffset(x = screenWidth - MinMenuWidth - 32.dp, y = (-8).dp)
 
-    DropdownMenu(
-        modifier = Modifier
-            .wrapContentSize()
-            .sizeIn(minWidth = MinMenuWidth),
-        offset = dropDownOffset,
-        expanded = isExpanded,
-        onDismissRequest = onDismiss
-    ) {
-        DropdownMenuItem(
-            onClick = onNavigateToRepresentation,
-            contentPadding = PaddingValues(start = DropdownMenuItemContentStartPadding)
+    MaterialTheme(shapes = MaterialTheme.shapes.copy(medium = RoundedCornerShape(8.dp))) {
+        DropdownMenu(
+            modifier = Modifier
+                .wrapContentSize()
+                .sizeIn(minWidth = MinMenuWidth),
+            offset = dropDownOffset,
+            expanded = isExpanded,
+            onDismissRequest = onDismiss
         ) {
-            Text(
-                text = stringResource(id = R.string.our_rule_menu_edit_representation),
-                style = HousTheme.typography.b2,
-                color = HousBlue,
-                modifier = Modifier.wrapContentSize()
-            )
-        }
-        Divider(
-            color = HousG2,
-            thickness = 1.dp,
-            modifier = Modifier.padding(horizontal = DropdownDividerPadding)
-        )
-        DropdownMenuItem(
-            onClick = onNavigateToGuide,
-            contentPadding = PaddingValues(start = DropdownMenuItemContentStartPadding)
-        ) {
-            Text(
-                text = stringResource(id = R.string.our_rule_menu_edit_guide),
-                style = HousTheme.typography.b2,
-                color = HousG6,
-                modifier = Modifier.wrapContentSize()
+            RuleDropDownMenuContent(
+                onNavigateToRepresentation = onNavigateToRepresentation,
+                onNavigateToGuide = onNavigateToGuide
             )
         }
     }
 }
 
+@Composable
+private fun RuleDropDownMenuContent(
+    onNavigateToRepresentation: () -> Unit,
+    onNavigateToGuide: () -> Unit
+) {
+    Column(Modifier.wrapContentSize()) {
+        Text(
+            text = stringResource(id = R.string.our_rule_menu_edit_representation),
+            style = HousTheme.typography.b2,
+            color = HousBlue,
+            modifier = Modifier.wrapContentSize().clickable(onClick = onNavigateToRepresentation)
+                .padding(start = 20.dp, end = 27.dp, top = 9.dp, bottom = 12.dp)
+        )
+        Divider(
+            color = HousG2,
+            thickness = 1.dp,
+            modifier = Modifier.wrapContentSize().padding(horizontal = DropdownDividerPadding)
+        )
+        Text(
+            text = stringResource(id = R.string.our_rule_menu_edit_guide),
+            style = HousTheme.typography.b2,
+            color = HousG6,
+            modifier = Modifier.wrapContentSize().clickable(onClick = onNavigateToGuide)
+                .padding(start = 20.dp, end = 27.dp, top = 8.dp, bottom = 12.dp)
+        )
+    }
+}
+
 @Preview(name = "MainRuleDropDown", showBackground = true)
 @Composable
-fun PreviewMainRuleDropDown() {
+private fun Preview1() {
+    HousTheme {
+        RuleDropDownMenuContent({}, {})
+    }
+}
+
+@Preview(name = "MainRuleDropDown", showBackground = true)
+@Composable
+private fun PreviewMainRuleDropDown() {
     HousTheme {
         Column(
             modifier = Modifier.fillMaxSize(),
