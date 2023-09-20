5acad805cd3cae79cf6107079e5e29af3a48ad86
이빵주 <yungju9982@naver.com>
2023-09-20 13:00:00 +0900

    #292 [feat] Bundle로 넘어온 값에 따른 레이아웃 처리

--- app/src/main/java/hous/release/android/presentation/withdraw/withdrawDone/WithdrawDoneFragment.kt
+++ app/src/main/java/hous/release/android/presentation/withdraw/withdrawDone/WithdrawDoneFragment.kt
@@ -5,26 +5,48 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentWithdrawDoneBinding
 import hous.release.android.presentation.login.LoginActivity
+import hous.release.android.presentation.withdraw.WithdrawActivity.Companion.FEEDBACK
+import hous.release.android.presentation.withdraw.WithdrawActivity.Companion.LOCATION
+import hous.release.android.presentation.withdraw.WithdrawActivity.Companion.WITHDRAW
 import hous.release.android.util.binding.BindingFragment
 import hous.release.android.util.extension.setOnSingleClickListener
 
 @AndroidEntryPoint
 class WithdrawDoneFragment :
     BindingFragment<FragmentWithdrawDoneBinding>(R.layout.fragment_withdraw_done) {
+    private val withdrawDoneViewModel by viewModels<WithdrawDoneViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = withdrawDoneViewModel
+        initLayout()
         initReturnBtnClickListener()
         initBackPressedCallback()
     }
 
+    private fun initLayout() {
+        arguments?.getString(LOCATION, "")?.let { location ->
+            withdrawDoneViewModel.initLayout(location)
+        }
+    }
+
     private fun initReturnBtnClickListener() {
         binding.tvWithdrawDoneReturn.setOnSingleClickListener {
-            startActivity(Intent(requireActivity(), LoginActivity::class.java))
-            requireActivity().finishAffinity()
+            when (withdrawDoneViewModel.layout.value) {
+                WITHDRAW -> {
+                    startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                    requireActivity().finishAffinity()
+                }
+                FEEDBACK -> {
+                    requireActivity().finish()
+                }
+                else -> throw IllegalArgumentException(withdrawDoneViewModel.layout.value)
+            }
         }
     }
 
