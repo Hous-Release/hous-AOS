27fe2b7f799be4f5205517aaf14e2b92d108dbe3
murjune <sis9221302@gmail.com>
2024-03-11 00:03:23 +0900

    feat : 피드백 보내기 이탈 다이얼로그

--- app/src/main/java/hous/release/android/presentation/feedback/feedback/FeedbackFragment.kt
+++ app/src/main/java/hous/release/android/presentation/feedback/feedback/FeedbackFragment.kt
@@ -3,22 +3,35 @@ package hous.release.android.presentation.feedback.feedback
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
 import hous.release.android.R
 import hous.release.android.databinding.FragmentFeedbackBinding
 import hous.release.android.util.KeyBoardUtil
 import hous.release.android.util.binding.BindingFragment
+import hous.release.android.util.dialog.ConfirmClickListener
+import hous.release.android.util.dialog.WarningDialogFragment
+import hous.release.android.util.dialog.WarningType
+import hous.release.android.util.extension.withArgs
 
 @AndroidEntryPoint
 class FeedbackFragment : BindingFragment<FragmentFeedbackBinding>(R.layout.fragment_feedback) {
     private val feedbackViewModel by activityViewModels<FeedbackViewModel>()
+    private lateinit var onBackPressedCallback: OnBackPressedCallback
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = feedbackViewModel
         initEditTextClearFocus()
         initBackBtnClickListener()
+        initBackBtnOnClick()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        onBackPressedCallback.remove()
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -29,9 +42,32 @@ class FeedbackFragment : BindingFragment<FragmentFeedbackBinding>(R.layout.fragm
         }
     }
 
+    private fun initBackBtnOnClick() {
+        requireActivity().onBackPressedDispatcher.apply {
+            addCallback {
+                showOutDialog()
+            }.also { callback ->
+                onBackPressedCallback = callback
+            }
+        }
+    }
+
     private fun initBackBtnClickListener() {
         binding.btnFeedbackBack.setOnClickListener {
-            requireActivity().finish()
+            showOutDialog()
         }
+    }
+
+    private fun showOutDialog() {
+        WarningDialogFragment().withArgs {
+            putSerializable(
+                WarningDialogFragment.WARNING_TYPE,
+                WarningType.WARNING_PEED_BACK
+            )
+            putParcelable(
+                WarningDialogFragment.CONFIRM_ACTION,
+                ConfirmClickListener(confirmAction = { requireActivity().finish() })
+            )
+        }.show(childFragmentManager, WarningDialogFragment.DIALOG_WARNING)
     }
 }
