68eb2e67a13684d16467b0f73e4a1e29c3a29357
murjune <sis9221302@gmail.com>
2024-03-18 00:38:32 +0900

    feat(Todo) : 추가 제한, 삭제 다이얼로그 영역 밖 터치시 꺼지지 않게

--- feature/todo/src/main/java/hous/release/feature/todo/detail/TodoDetailScreen.kt
+++ feature/todo/src/main/java/hous/release/feature/todo/detail/TodoDetailScreen.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
@@ -110,14 +111,22 @@ fun TodoDetailScreen(
                     bottomSheetState.hide()
                 }
             },
-            onDismissRequest = { isDeleteTodo = false }
+            onDismissRequest = { isDeleteTodo = false },
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            )
         )
     }
     if (isLimitTodo) {
         HousLimitDialog(
             title = stringResource(R.string.todo_limit_title),
             content = stringResource(R.string.todo_limit_content),
-            onDismissRequest = { isLimitTodo = false }
+            onDismissRequest = { isLimitTodo = false },
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            )
         )
     }
 
