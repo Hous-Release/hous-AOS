723c2f220c1f413c2ba07519799e1c03419ab820
murjune <sis9221302@gmail.com>
2024-03-18 00:51:50 +0900

    style : ktFormat

--- app/src/main/java/hous/release/android/presentation/our_rules/component/main/MainRuleContent.kt
+++ app/src/main/java/hous/release/android/presentation/our_rules/component/main/MainRuleContent.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +20,6 @@ import hous.release.designsystem.component.HousTextField
 import hous.release.designsystem.component.HousTextFieldMode
 import hous.release.designsystem.theme.HousTheme
 import hous.release.domain.entity.rule.Rule
-import hous.release.feature.todo.R
 
 @Composable
 fun MainRuleContent(
